1b3d60061e83287c8ab94837dfe93534af638058
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-01 21:41:24 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.42%) - LeetHub

---
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minSteps(int n) {
+        int ans = 0;
+        for(int i=2;i*i<=n;){
+            if(n%i == 0){
+                ans += i;
+                n/=i;
+            }
+            else{
+                i++;
+            }
+        }
+        if(n!=1){
+            ans+=n;
+        }
+        return ans;
+    }
+};
