087469b67685cf9dad0688107a40258b87a79704
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-03 06:46:40 +0530

    Time: 18 ms (97.28%), Space: 13.6 MB (73.69%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& edge) {
+        int v = numCourses, e = edge.size();
+        
+        vector<int> graph [v];
+        for(int i=0;i<e;i++){
+            graph[edge[i][0]].push_back(edge[i][1]);
+        }
+        
+        int indeg [v];
+        fill(indeg,indeg+v,0);
+        
+        vector<int>topoSort;
+        
+        for(auto edgeList: graph){
+            for(auto u: edgeList){
+                indeg[u]++;
+            }
+        }
+        
+        queue<int> que;
+        for(int i=0;i<v;i++){
+            if(indeg[i] == 0){
+                que.push(i);
+            }
+        }
+        int level = 0;
+        while(que.size()){
+            int sz = que.size();
+            while(sz--){
+                int v = que.front();
+                topoSort.push_back(v);
+                que.pop();
+                for(auto child:graph[v]){
+                    if(--indeg[child] == 0){
+                        que.push(child);
+                    }
+                }
+            }
+            level ++;
+        }
+        reverse(topoSort.begin(), topoSort.end());
+        if(topoSort.size() == v){
+            return topoSort;
+        }
+        return {};
+    }
+};
