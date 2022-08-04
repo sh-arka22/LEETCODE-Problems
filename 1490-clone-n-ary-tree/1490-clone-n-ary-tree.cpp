d1843a1f7d81c25af0ef93a46e6e321631fa1b36
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-04 17:55:28 +0530

    Time: 119 ms (51.18%), Space: 175.3 MB (59.06%) - LeetHub

---
+++ 1490-clone-n-ary-tree/1490-clone-n-ary-tree.cpp
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Pair{
+public:
+    Node *Old, *New;
+    Pair(Node* Old, Node* New){
+        this->Old = Old;
+        this->New = New;
+    }
+};
+class Solution {
+public:
+    Node* cloneTree(Node* root) {
+        if(root == NULL)
+            return NULL;
+        queue<Pair>que;
+        Node *copy = NULL;
+        que.push(Pair(root,copy));
+        while(que.size()){
+            int sz = que.size();
+            while(sz--){
+                auto top = que.front(); que.pop();
+                Node *node = new Node(top.Old->val);
+                if(top.New == NULL){
+                    copy = node;
+                }
+                else if(top.New){
+                    top.New->children.push_back(node);
+                }
+                for(auto child: top.Old->children){
+                    que.push(Pair(child,node));
+                }
+            }
+        }
+        return copy;
+    }
+};
