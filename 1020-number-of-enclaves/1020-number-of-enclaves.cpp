d71f52b54a8344985a79eb428744f41e27edfeb1
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-05 00:52:39 +0530

    Time: 108 ms (39.55%), Space: 21.8 MB (56.88%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -21,14 +21,6 @@ public:
             }
         }
         
-        
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         if(grid[i][j]==1){
-        //             dfs(grid, i, j, n, m);
-        //         }
-        //     }
-        // }
         int cnt = 0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
