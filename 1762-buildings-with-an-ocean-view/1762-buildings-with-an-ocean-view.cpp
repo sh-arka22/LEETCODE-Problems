1270d5df3043fe6c424112fd8c8d394cf50eb38e
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-07 05:03:40 +0530

    Time: 147 ms (95.08%), Space: 68.9 MB (32.57%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     vector<int> findBuildings(vector<int>& heights) {
         int n = heights.size();
-        int suff[n];
-        
-        suff[n-1] = heights[n-1];
-        
-        for(int i=n-2;i>=0;i--){
-            suff[i] = max(suff[i+1],heights[i]);
-        }
-        
-        for(int i=0;i<n-1;i++){
-            if(heights[i]<=suff[i+1]){
-                suff[i] = -1;
-            }
-        }
-        
+        int maxi = 0;
         vector<int>ans;
+        for(int i=n-1;i>=0;i--){
+            if(heights[i]<=maxi){
+                heights[i] = -1;
+            }
+            maxi = max(maxi,heights[i]);
+        }
         for(int i=0;i<n;i++){
-            if(suff[i] != -1){
+            if(heights[i] != -1){
                 ans.push_back(i);
             }
         }
