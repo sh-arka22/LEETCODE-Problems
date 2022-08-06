4fa3bb84b0c1f7f040296739214d2e06ca4b1c41
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-06 17:38:09 +0530

    Time: 276 ms (44.04%), Space: 47.3 MB (67.41%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,53 +1,47 @@
 class Solution {
+private:
+    vector<int>vis;
+    int dfs(vector<vector<int>>& graph, int v){
+        if(vis[v] == 1){
+            vis[v] = -1;
+            return -1;
+        }
+        if(vis[v] == 2){
+            return 2;
+        }
+        vis[v] = 1; //marked the starting path
+        for(int child:graph[v]){
+            if(dfs(graph,child) == -1){
+                vis[v] = -1;
+                vis[child] = -1;
+                return -1;
+            }
+        }
+        return vis[v] = 2;
+    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-
-	int n = graph.size();
-	vector<vector<int>> adj(n);
-	vector<int> indegree(n, 0);
-	int i = 0;
-	
-	// Reverse Edges of graph
-	for(auto& g : graph) {
-		for(auto& it : g) {
-			adj[it].push_back(i);
-			indegree[i]++;
-		}
-		i++;
-	}
-
-	// Topo Sort
-	queue<int> q;
-
-	for(int i=0; i<n; i++) {
-		if(indegree[i] == 0) q.push(i);
-	}
-
-	
-        vector<vector<int>>level;
+        // 0-> unvisted   1-> assume visisted but not in cycle    -1-> visited and in cycle  2->visited and sure not a part of cycle
         vector<int>ans;
-    while(q.size()){
-        int sz = q.size();
-        vector<int> safeNodes;
-        while(sz--){
-            int node = q.front(); q.pop();
-            safeNodes.push_back(node);
-            ans.push_back(node);
-            for(auto& adjnode : adj[node]) {
-			    if(--indegree[adjnode] == 0) 
-                    q.push(adjnode);
-		    }
-        }
-        level.push_back(safeNodes);
-    }
-        for(auto v:level){
-            for(auto vv:v){
-                cout<<vv<<" ";
+        int n = graph.size();
+        vis.resize(n,0);
+        for(int v=0;v<n;v++){
+            if(vis[v] == 0){
+                int flag = dfs(graph,v);
+                // if(dfs(graph,v) == 2){
+                //     ans.push_back(v);
+                // }   
             }
-            cout<<endl;
         }
-	sort(ans.begin(), ans.end());// merge k sorted list
-
-	return ans;
-}
+        
+        for(int i = 0;i<n;i++){
+            if(vis[i]==2){
+                ans.push_back(i);
+            }
+        }
+        for(auto it:vis){
+            cout<<it<<" ";
+        }
+        return ans;
+    }
 };
