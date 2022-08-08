577672160787446b8e834d7c1290df2cec153eaa
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-09 04:37:02 +0530

    Time: 193 ms (65.88%), Space: 8.1 MB (18.79%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
-    // Time Complexity:- O(26*26*N)
-    // Space Complexity:- O(26)
     int largestVariance(string s) {
-        int ans = 0;
-        vector<int> freq(26);
-        for(auto& c:s){
-            freq[c-'a']++;
-        }
-        for(char ch1='a';ch1<='z';ch1++){
-            for(char ch2='a';ch2<='z';ch2++){
-                if(ch1==ch2 or !freq[ch1-'a'] or !freq[ch2-'a']){
-                    continue;
+    int total = 0;
+    unordered_set<char> unique(begin(s), end(s));
+
+    for (char a: unique) {
+        for (char b: unique) {
+            if (a == b) continue;
+
+            int countA = 0, countB = 0;
+            for (auto ch: s) {
+                countA += (ch == a); //1
+                countB += (ch == b); //1
+                // cout<<countA<<ch<<countB<<endl;
+                if (countA - countB < 0) {
+                    countA = 0;
+                    countB = 0;
                 }
-                for(int rev=1;rev<=2;rev++){
-                   int cnt1 = 0,cnt2 = 0;
-                    for(auto& c:s){
-                        cnt1 += c==ch1;
-                        cnt2 += c==ch2;
-                        if(cnt1<cnt2){
-                            cnt1 = cnt2 = 0;
-                        }
-                        if(cnt1>0 and cnt2>0){
-                            ans = max(ans,cnt1-cnt2);
-                        }
-                    } 
-                    reverse(s.begin(),s.end());
-                }
+                // cout<<ch<<total<<" ";
+                total = max(total, countB > 0 ? countA - countB : countA - countB - 1);
                 
             }
         }
-        return ans;
+    }
+    return total;
     }
 };
