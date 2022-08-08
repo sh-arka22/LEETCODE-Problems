eca43d145dde67b8d174d73479ce19f1ae846f85
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-09 03:22:48 +0530

    Time: 183 ms (70.68%), Space: 8.1 MB (18.79%) - LeetHub

---
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int largestVariance(string s) {
+    int total = 0;
+    unordered_set<char> unique(begin(s), end(s));
+
+    for (char a: unique) {
+        for (char b: unique) {
+            if (a == b) continue;
+
+            int countA = 0, countB = 0;
+            for (auto ch: s) {
+                countA += (ch == a);
+                countB += (ch == b);
+
+                total = max(total, countB > 0 ? countA - countB : countA - countB - 1);
+
+                if (countA - countB < 0) {
+                    countA = 0;
+                    countB = 0;
+                }
+            }
+        }
+    }
+    return total;
+    }
+};
