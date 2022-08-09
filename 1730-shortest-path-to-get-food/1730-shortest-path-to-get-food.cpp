068e6170328e318f808373348076410b181290c5
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-09 19:30:25 +0530

    Time: 99 ms (75.66%), Space: 17.6 MB (85.09%) - LeetHub

---
+++ 1730-shortest-path-to-get-food/1730-shortest-path-to-get-food.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int dir[4][2] = {{1,0},{0,1},{-1,0},{0,-1}};
+    int getFood(vector<vector<char>>& grid) {
+        
+        int n = grid.size(), m = grid[0].size();
+        
+        queue<pair<int,int>>que;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == '*'){
+                    que.push({i,j});
+                    break;
+                }
+            }
+        }
+        
+        int rad = 0;
+        while(que.size()){
+            int sz = que.size();
+            rad++;
+            while(sz--){
+                auto top = que.front(); que.pop();
+                int r = top.first, c = top.second;
+                for(auto &[dr,dc]:dir){
+                    if(r+dr >=0 and r+dr < n and c+dc >= 0 and c+dc < m and grid[r+dr][c+dc] == '#')
+                            return rad;
+                    if(r+dr >=0 and r+dr < n and c+dc >= 0 and c+dc < m and grid[r+dr][c+dc] == 'O'){
+                        que.push({r+dr,c+dc});
+                        grid[r+dr][c+dc] = 'X';
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
