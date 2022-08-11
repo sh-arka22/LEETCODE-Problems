c1161a9af8607bc9f57d2caccba1ee79f4b74341
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-11 20:50:51 +0530

    Time: 30 ms (51.73%), Space: 23.1 MB (98.07%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
-    
-    
-    int backtrackArea(vector<vector<int>>& grid,int i,int j){
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<int>> dir={{0,-1},{-1,0},{0,1},{1,0}};
-        
-        if(i>=n or j>=m or i<0 or j<0) return 0;
-        if(grid[i][j] == 0) return 0;
-        grid[i][j] = 0;
-        
-        int faithArea = 0;
-        for(int r=0;r<dir.size();r++){
-            faithArea += backtrackArea(grid, i+dir[r][0],j+dir[r][1]);
-        }
-        return 1+faithArea;
+    int parr[3000], size[3000];
+    int findParr(int v){
+        return parr[v] == v ? v : (parr[v] = findParr(parr[v]));
     }
-    
-    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        int maxArea = 0;
+        // GRAPH SOLUTION USING (_Union Find_)
+        int maxi = 0;
+        int n = grid.size(), m = grid[0].size();
+        if(n == 0)
+            return 0;
+        for(int i=0;i<n*m;i++){
+            parr[i] = i;
+            size[i] = 1;
+        }
+        int dir[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                maxArea = max(maxArea,backtrackArea(grid,i,j));
+                if(grid[i][j] == 1){
+                    int p1 = findParr(i*m+j);
+                    for(auto &[dr,dc]:dir){
+                        int r = i+dr, c = j+dc;
+                        if(r>=0 and r<n and c>=0 and c<m and grid[r][c] == 1){
+                            //means i am a edge of {i,j}
+                            int p2 = findParr(r*m + c);
+                            if(p1 != p2){
+                                parr[p2] = p1;
+                                size[p1] += size[p2];
+                            }
+                        }
+                    }
+                    maxi = max(maxi, size[p1]);
+                }
             }
         }
-        return maxArea;
+        return maxi;
     }
 };
