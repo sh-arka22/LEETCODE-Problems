cbd6948a90694469eb927ed32d28575ad3bee9a7
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-11 06:40:03 +0530

    Time: 20 ms (63.52%), Space: 17.5 MB (70.06%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -5,8 +5,8 @@ public:
         for(int i=0;i<asteroids.size();i++){
             if(asteroids[i] > 0 || ans.empty() || ans.back() < 0)
                 ans.push_back(asteroids[i]);
-            else if(ans.back() <= -asteroids[i]){
-                if(ans.back() < -asteroids[i])
+            else if(ans.back() <= abs(asteroids[i])){
+                if(ans.back() < abs(asteroids[i]))
                     i--;
                 ans.pop_back();
             }
