f69073eda35bd428cd4bd9c39003cc6976faef7e
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-13 03:03:47 +0530

    Time: 54 ms (54.50%), Space: 12.8 MB (33.06%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,29 +1,37 @@
 class Solution {
-private:
-    int dir[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
-    void dfs(vector<vector<char>>& grid, int r, int c, int n, int m){
-        grid[r][c] = '0';
-        for(auto&[dr,dc]:dir){
-            if(r+dr<0 or r+dr>=n or c+dc<0 or c+dc>=m or grid[r+dr][c+dc] == '0')
-                continue;
-            dfs(grid, r+dr, c+dc, n, m);
-        }
-    }
 public:
+    const static int N = 1e5;
+    int parr[N];
+    int dir[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};
+    int findParr(int v){
+        return parr[v] == v ? v : (parr[v] = findParr(parr[v]));
+    }
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.size() == 0 or grid[0].size() == 0)
-            return 0;
-        int cnt = 0;
-        int n = grid.size();
-        int m = grid[0].size();
+        
+        int n = grid.size(), m = grid[0].size();
+        
+        int isLand = 0;
+        for(int i=0;i<n*m;i++) parr[i] = i, isLand += (grid[i/m][i%m] == '1');
+        cout<<isLand;
+        
         for(int i=0;i<n;i++){
-            for(int j =0;j<m;j++){
-                if(grid[i][j] == '0')
-                    continue;
-                cnt++;
-                dfs(grid,i,j,n,m);
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == '0') continue;
+                
+                int p1 = findParr(i*m+j);
+                for(auto &[dr,dc]:dir){
+                    int r = dr+i, c = dc+j;
+                    if(r>=0 and r<n and c>=0 and c<m and grid[r][c] == '1'){
+                        int p2 = findParr(r*m+c);
+                        
+                        if(p1 != p2){
+                            parr[p2] = p1;
+                            isLand--;
+                        }
+                    }
+                }
             }
         }
-        return cnt;
+        return isLand;
     }
 };
