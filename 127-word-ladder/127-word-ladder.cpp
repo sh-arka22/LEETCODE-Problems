3f2d050113787c8e72caddfa7d6c67649c8c54f2
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-14 00:29:40 +0530

    Time: 663 ms (42.92%), Space: 14 MB (69.41%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        queue<string> PendingNodes;
+        PendingNodes.push(beginWord);
+        int level = 0;
+        set<string> GivenWords;
+        for(auto x : wordList){
+            GivenWords.insert(x);
+        }
+        
+        while(!PendingNodes.empty()){
+            int size = PendingNodes.size();
+            while(size-->0){
+                string front = PendingNodes.front();
+                if(front == endWord){
+                    return level+1;
+                }
+                PendingNodes.pop();
+                string temp = front;
+                for(int i = 0 ; i < front.size() ; i++){
+                    for(char j = 'a' ; j <= 'z' ; j++){
+                        temp[i] = j;
+                        if((temp != front) && (GivenWords.find(temp) != GivenWords.end())){
+                            PendingNodes.push(temp);
+                            GivenWords.erase(temp);
+                        }
+                    }
+                    temp[i] = front[i];
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+};
