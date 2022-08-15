4e14e4316330a3b88aa00e23f4b2dd26573f221e
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-16 04:08:29 +0530

    Time: 150 ms (25.36%), Space: 20.2 MB (23.80%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,45 +1,19 @@
 class Solution {
 public:
-    /*
-    DP SOLUTION
-    */
-    int recur(vector<vector<int>> &g, int s, int d, int k,vector<vector<vector<int>>> &dp) {
-    if(s == d)
-        return 0;
-    if(k == 0 and g[s][d] != INT_MAX)
-        return g[s][d];
-    if(k <= 0)
-        return INT_MAX;
-    
-    if(dp[s][d][k] != -1)
-        return dp[s][d][k];
-    
-    int res = INT_MAX;
-    
-    for(int i = 0; i < g.size(); i++) {
-        if(g[s][i] != INT_MAX and s != i) {
-            int temp = recur(g, i, d, k-1, dp);
-            if(temp != INT_MAX) {
-                res = min(res, g[s][i] + temp);
-            }
+    set<tuple<int,int,int>>st;
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>>graph(n);
+        for(const auto&f:flights)   graph[f[0]].push_back({f[1],f[2]});
+        
+        st.insert(make_tuple(0,src,k+1));
+        
+        while(st.size()){
+            auto[wsf, parr, stops] = *st.begin();
+            st.erase(st.begin());
+            if(parr == dst) return wsf;
+            if(!stops) continue;
+            for(auto &[u,w]:graph[parr]) st.insert(make_tuple(wsf+w,u,stops-1));
         }
-    }
-    dp[s][d][k] = res;
-    return res;
-}
-
-int findCheapestPrice(int n, vector<vector<int>>& flights, int s, int d, int k) {
-    
-    vector<vector<int>> g(n, vector<int>(n, INT_MAX));
-    vector<vector<vector<int>>> dp(n, vector<vector<int>>(n, vector<int>(k+1, -1)));
-    
-    for(vector<int> edge: flights) {
-        g[edge[0]][edge[1]] = edge[2];
-    }
-    
-    int res = recur(g, s, d, k, dp);
-    if(res == INT_MAX)
         return -1;
-    return res;
-}
+    }
 };
