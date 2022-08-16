3d02929aa184807e81dfc175ef0b2e67291a81f2
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-16 16:02:41 +0530

    Time: 132 ms (27.00%), Space: 20.3 MB (25.01%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    //USING DIJKTRAS
-    set<tuple<int,int,int>>st;
+    //WITHOUT DIJECTRAS
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>>>graph(n);
+       vector<vector<pair<int,int>>>graph(n);
         for(const auto&f:flights)   graph[f[0]].push_back({f[1],f[2]});
         
+        set<tuple<int,int,int>>st;
+        
         st.insert(make_tuple(0,src,k+1));
-        
+        vector<int>vis(n,0);
         int mini = INT_MAX;
-        
         while(st.size()){
             auto[wsf, parr, stops] = *st.begin();
             st.erase(st.begin());
@@ -17,6 +17,11 @@ public:
             /**
             Added extra
             */
+            // if(vis[parr]) continue;
+            // if(stops>=0)
+            //     vis[parr] = 1;
+            // else continue;
+            
             if(parr == dst) return mini = min(mini,wsf) ;
             if(!stops) continue;
             
@@ -24,7 +29,9 @@ public:
             Not required vis bcs it is directed graph
             */
             
-            for(auto &[u,w]:graph[parr]) st.insert(make_tuple(wsf+w,u,stops-1));
+            for(auto &[u,w]:graph[parr]){
+                st.insert(make_tuple(wsf+w,u,stops-1));
+            }
         }
         return mini == INT_MAX ? -1 : mini;
     }
