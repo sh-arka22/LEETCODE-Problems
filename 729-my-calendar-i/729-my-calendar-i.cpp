bc778eea76d8843075456427dbdd4a5a3a7f655c
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-19 05:29:43 +0530

    Time: 87 ms (98.74%), Space: 38.9 MB (35.95%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,23 +1,13 @@
 class MyCalendar {
 private:
-    set<pair<int, int>> calendar;
-
+    map<int,int>events;
 public:
+    MyCalendar() {   
+    }
     bool book(int start, int end) {
-        const pair<int, int> event{start, end};
-        const auto nextEvent = calendar.lower_bound(event);
-        if (nextEvent != calendar.end() && nextEvent->first < end) {
-            return false;
-        }
-
-        if (nextEvent != calendar.begin()) {
-            const auto prevEvent = prev(nextEvent);
-            if (prevEvent->second > start) {
-                return false;
-            }
-        }
-
-        calendar.insert(event);
+        auto next = events.upper_bound(start);
+        if(next != events.end() &&   (*next).second < end)return false;
+        events.insert({end,start});
         return true;
     }
 };
