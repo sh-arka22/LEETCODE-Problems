da0537e2cab71dc13171ac6299c739de08522545
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-20 02:28:19 +0530

    Time: 125 ms (85.93%), Space: 33.7 MB (78.81%) - LeetHub

---
+++ 721-accounts-merge/721-accounts-merge.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int *parr;
+    unordered_map<string,int>emailId;
+    unordered_map<string,string>emailName;
+    int findParr(int v){
+        return parr[v] == v ? v : (parr[v] = findParr(parr[v]));
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        parr = new int[1001];
+        int n = accounts.size();
+        for(int i=0;i<n;i++) parr[i] = i;
+        
+        for(int i=0;i<n;i++){
+            string name = accounts[i][0];
+            for(int j=1;j<accounts[i].size();j++){
+                if(emailId.count(accounts[i][j]) == 0){
+                    emailId[accounts[i][j]] = i;
+                }
+                else{
+                    int p1 = findParr(i);
+                    int p2 = findParr(emailId[accounts[i][j]]);
+                    parr[p2] = p1;
+                }
+            }
+        }
+        
+        unordered_map<int,vector<string>>idEmail;
+        for(auto &[email,id]:emailId){
+            int par = findParr(id);
+            idEmail[par].push_back(email);
+        }
+        
+        vector<vector<string>>ans;
+        for(auto [id,emails]:idEmail){
+            // vector<string> email = it.second;
+            sort(emails.begin(),emails.end());
+            emails.insert(emails.begin(), accounts[id][0]);
+            ans.push_back(emails);
+        }
+        return ans;
+    }
+};
