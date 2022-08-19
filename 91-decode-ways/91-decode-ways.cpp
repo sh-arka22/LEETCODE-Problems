51a51b56000bbd786622064ebd9bf951207e9f59
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-19 18:25:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (49.91%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     int *dp;
-    int recc(string s, int idx){
-        if(s[idx] == '0') return dp[idx] = 0;
-        if(idx == 0) return dp[idx] = 1;
+    int tabulation(string s, int IDX){
+    for(int idx=0; idx<=IDX; idx++){
+        if(s[idx] == '0'){
+            dp[idx] = 0;
+            continue;
+        }
+        if(idx == 0){
+            dp[idx] = 1;
+            continue;
+        }
         
-        if(dp[idx] != -1) return dp[idx];
+        // if(dp[idx] != -1) return dp[idx];
         char ch1 = s[idx-1];
-        int cnt = recc(s, idx-1);
+        int cnt = dp[idx-1];
         
         if(idx>=2){
             char ch2 = s[idx-2];
             int num = (ch2-'0')*10 + (ch1-'0');
             if(num<=26 and num>0)
-                cnt+=recc(s,idx-2);
+                cnt+=dp[idx-2];
         }
-        return dp[idx] = cnt;
+        dp[idx] = cnt;
     }
+    return dp[IDX];
+}
     int numDecodings(string s) {
         int n = s.size();
         dp = new int[n+1];
         fill(dp,dp+n+1, -1);
-        int ans = recc(s,n);
+        int ans = tabulation(s,n);
         delete[] dp;
         return ans;
     }
