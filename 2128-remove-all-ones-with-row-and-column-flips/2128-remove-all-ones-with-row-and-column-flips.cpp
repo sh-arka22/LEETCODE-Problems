e4d87dfdb6f5c1f55c549c3a1ebfcfa53957dd25
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-20 06:07:36 +0530

    Time: 68 ms (66.12%), Space: 24.5 MB (76.89%) - LeetHub

---
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    
+public:
+    bool removeOnes(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+		// If any cell on the first row has 1, flipping that column. 
+        
+        for(int i=0; i<m; i++){
+            if(grid[0][i] == 1){
+                // flipping that column
+                for(int j=0; j<n; j++){
+                    grid[j][i] = !grid[j][i];
+                }
+            }
+        }
+		
+		// Now, all other rows should be either all 0's or all 1's. If not, than that's impossible to find a solution 
+        
+        for(int i=0; i<n; i++){
+            int sum = 0;
+            for(int j=0; j<m; j++){
+                sum += grid[i][j];
+            }
+            
+            if(sum != 0 && sum != m){
+                return false;
+            }
+        }
+        
+        return true;
+        
+    }
+};
