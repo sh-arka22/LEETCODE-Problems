9e8533a3df3615ca30335b0f6fdcee792710afa4
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-20 06:08:19 +0530

    Time: 66 ms (70.29%), Space: 24.4 MB (76.89%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
@@ -8,12 +8,13 @@ public:
 		// If any cell on the first row has 1, flipping that column. 
         
         for(int i=0; i<m; i++){
-            if(grid[0][i] == 1){
+            if(grid[0][i] == 0) continue;
+            
                 // flipping that column
                 for(int j=0; j<n; j++){
                     grid[j][i] = !grid[j][i];
                 }
-            }
+            
         }
 		
 		// Now, all other rows should be either all 0's or all 1's. If not, than that's impossible to find a solution 
