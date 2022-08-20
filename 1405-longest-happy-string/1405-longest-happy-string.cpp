9a8b7f9c53e461e7bb41bda9c0149908ebfc0cae
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-21 02:36:52 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (71.63%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+    string longestDiverseString(int a, int b, int c) {
+        multiset<pair<int , char>> pq;
+
+         if(a > 0)pq.insert({a , 'a'});
+         if(b > 0)pq.insert({b , 'b'});
+         if(c > 0)pq.insert({c , 'c'});
+
+         string t;
+         while(pq.size()>1){
+
+             auto it = --pq.end();
+             char mostfreq = it -> second;
+             int freq      = it -> first;
+
+             auto it2 = pq.begin();
+             char nextfreq = it2 -> second;
+             int freq2     = it2 -> first;
+
+             pq.erase(it);
+             pq.erase(it2);
+
+             if(freq > 1){
+                 t += mostfreq;
+                 t += mostfreq;
+                 freq -= 2;
+                 if(freq){
+                      pq.insert({freq , mostfreq});
+                 }
+             }
+             else if(freq == 1){
+                  t += mostfreq;
+             }
+
+             if(freq2 > 1){
+                 if(freq2 < freq){
+                    t += nextfreq;
+                    freq2 -= 1;
+                 }else{
+                     t += nextfreq;
+                     t += nextfreq;
+                     freq2 -= 2;
+                 }
+                 if(freq2){
+                      pq.insert({freq2 , nextfreq});
+                 }
+             }
+             else if(freq2== 1){
+                  t += nextfreq;
+             }
+         }
+
+         if(pq.size()){
+              auto it = pq.begin();             
+              int freq = it -> first;
+              char c = it -> second;
+
+              pq.erase(it);
+
+              if(freq > 1){
+                  t += c;
+                  t += c;
+              }
+              else if(freq == 1){
+                 t += c;
+              }
+         }
+         return t;
+    }
+};
