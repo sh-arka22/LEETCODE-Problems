d9113d43c4b730584eb48d0282004b37bff361c2
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-22 10:02:33 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (62.83%) - LeetHub

---
+++ 1138-alphabet-board-path/1138-alphabet-board-path.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string alphabetBoardPath(string target) {
+        int i = 0, j = 0;
+        string ans = "";
+        
+        for(char ch: target){
+            int r = (ch-'a')/5, c = (ch-'a')%5;
+            while(c !=j or r != i){
+                if(c>=j){
+                if(r>i){
+                    ans += 'D';
+                    i++;
+                }
+                else if(r<i){
+                    ans += 'U';
+                    i--;
+                }
+                else {
+                    ans += 'R';
+                    j++;
+                    }
+                }
+                else{
+                    ans += 'L';
+                    j--;
+                }
+            }
+            ans += '!';
+        }
+        return ans;
+    }
+};
