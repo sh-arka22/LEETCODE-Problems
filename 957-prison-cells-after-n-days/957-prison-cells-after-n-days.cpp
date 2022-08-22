8af85ce8b18c11b356b295824828c1d3d70d9711
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-23 00:36:22 +0530

    Time: 8 ms (52.03%), Space: 12.5 MB (20.79%) - LeetHub

--- 957-prison-cells-after-n-days/957-prison-cells-after-n-days.cpp
+++ 957-prison-cells-after-n-days/957-prison-cells-after-n-days.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> prisonAfterNDays(vector<int>& cells, int n) {
         vector<int>cell(8,0);
         vector<vector<int>>seen;
-        for(int i=0;i<=n-1;i++){
+        for(int i=0;i<=64;i++){
             for(int i = 1; i <= 6; i++){
                 cell[i] = cells[i-1] == cells[i+1];
             }
