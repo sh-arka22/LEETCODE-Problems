c1d9def36de203c74edd414682b578c1f500659f
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-25 02:26:27 +0530

    Time: 25 ms (22.50%), Space: 9.4 MB (97.98%) - LeetHub

--- 685-redundant-connection-ii/685-redundant-connection-ii.cpp
+++ 685-redundant-connection-ii/685-redundant-connection-ii.cpp
@@ -1,50 +1,77 @@
-class Dsu{
-    public:
-    vector<int> p;
-    Dsu(int n){
-        p.resize(n);
-        iota(p.begin(),p.end(),0);
-    }
-    int get(int n){
-        if(p[n] == n) return n;
-        return p[n] = get(p[n]);
-    }
-    
-    bool unite(int a,int b){
-        int p1 = get(a);
-        int p2 = get(b);
-        if(p1 == p2) return false;
-        p[p2] = p1;
-        return true;
-    }
-};
-
 class Solution {
 public:
-    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
-        int n = edges.size();
-        vector<int> Indegree(n+1,0);
-        for(int i = 0 ; i < n ; i++){
-            Indegree[edges[i][1]]++;
-        }
-        vector<vector<int>> LastNodes;
-        for(int i = 0 ; i < edges.size() ; i++){
-            if(Indegree[edges[i][1]] > 1){
-                LastNodes.push_back(edges[i]);
-            }
-        }
-        Dsu d(n+1);
+    const static int N = 1e3+5;
+    int parr[N];
+    int size[N];
+    int findParr(int v){
+        return parr[v] == v ? v : (parr[v] = findParr(parr[v]));
+    }
+    bool union_(int v, int u){
+        int p1 = findParr(v);
+        int p2 = findParr(u);
         
-        for(int i = 0 ; i < n ; i++){
-            if(LastNodes.size() > 0 && (edges[i] == LastNodes[0] || edges[i] == LastNodes[1])){
-                continue;
+        if(p1 != p2){
+            if(size[p1]>size[p2]){
+                parr[p2] = p1;
+                size[p1] += size[p2];
             }
-            if(!d.unite(edges[i][0],edges[i][1]))
-                return edges[i];
+            else {
+                parr[p1] = p2;
+                size[p2] += size[p1];
+            }
+            return false; // no cycle
         }
-        if(!d.unite(LastNodes[0][0],LastNodes[0][1]))
-            return LastNodes[0];
-        else
-            return LastNodes[1];
+        return true;
+    }
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        
+        int vn = edges.size();
+        int indegree[vn+1];
+        fill(indegree, indegree+vn+1, -1);
+        
+        vector<int> mayBe1 = {-1,-1} ,mayBe2 = {-1,-1};
+        
+        for(auto &edge:edges){
+            int v = edge[0];
+            int u = edge[1];
+            
+            if(indegree[u] == -1){
+                indegree[u] = v; // storing the node caused the indegree
+            }
+            else{
+                mayBe1 = {v,u};
+                mayBe2 = {indegree[u],u};
+                break;
+            }
+        }
+        
+        for(int i=1;i<=vn;i++){
+            parr[i] = i;
+            size[i] = 1;
+        }
+        
+        for(auto &edge:edges){
+            if(mayBe1 == edge) continue;
+            
+            int v = edge[0], u = edge[1];
+            
+            bool hasCycle = union_(v, u);
+            
+            if(hasCycle){
+                if(mayBe1[0] == -1){
+                    /**
+                    case 2
+                    */
+                    return edge;
+                }
+                else{
+                    /**
+                    
+                    */
+                    return mayBe2;
+                }
+            }
+        }
+        return mayBe1;
     }
 };
