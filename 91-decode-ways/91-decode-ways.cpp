816acd182720e66ac7c8a2aa01d868843199854e
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-28 21:32:14 +0530

    Time: 5 ms (33.67%), Space: 8.3 MB (9.15%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     int *dp;
-    const int tabulation(string s, int IDX){
-    for(int idx=0; idx<=IDX; idx++){
-        if(s[idx] == '0'){
-            dp[idx] = 0;
-            continue;
-        }
-        if(idx == 0){
-            dp[idx] = 1;
-            continue;
-        }
-        
-        // if(dp[idx] != -1) return dp[idx];
-        char ch1 = s[idx-1];
-        int cnt = dp[idx-1];
-        
-        if(idx>=2){
-            char ch2 = s[idx-2];
-            int num = (ch2-'0')*10 + (ch1-'0');
-            if(num<=26 and num>0)
-                cnt+=dp[idx-2];
-        }
-        dp[idx] = cnt;
+    int recc(string s, int idx){
+    if(s[idx] == '0') return dp[idx] = 0;
+    if(idx == 0) return dp[idx] = 1;
+    
+    if(dp[idx] != -1) return dp[idx];
+    char ch1 = s[idx-1];
+    int cnt = recc(s, idx-1);
+    
+    if(idx>=2){
+        char ch2 = s[idx-2];
+        int num = (ch2-'0')*10 + (ch1-'0');
+        if(num<=26 and num>0)
+            cnt+=recc(s,idx-2);
     }
-    return dp[IDX];
+    return dp[idx] = cnt;
 }
     int numDecodings(string s) {
-        int n = s.size();
-        dp = new int[n+1];
-        fill(dp,dp+n+1, -1);
-        int ans = tabulation(s,n);
-        delete[] dp;
-        return ans;
-    }
+    int n = s.size();
+    dp = new int[n+1];
+    fill(dp,dp+n+1, -1);
+    int ans = recc(s,n);
+    delete[] dp;
+    return ans;
+}
 };
