8544d502640337ca74242282483831dc2d9c9bd9
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-31 02:35:49 +0530

    Time: 2509 ms (5.00%), Space: 24.2 MB (50.17%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,63 +1,63 @@
 class Solution {
 public:
-    const static int N = 2e3+1;
-int dp[N][N];
-string removeStars(string &str) {
-    if (str.length() == 0)
-        return str;
+const static int N = 2e3+1;
+    int dp[N][N];
+    string removeStars(string &str) {
+        if (str.length() == 0)
+            return str;
 
-    string sb = "";
-    sb+=str[0];
+        string sb = "";
+        sb+=str[0];
 
-    int i = 1;
-    while (i < str.length()) {
-        while (i < str.length() && sb[sb.length() - 1] == '*' && str[i] == '*')
+        int i = 1;
+        while (i < str.length()) {
+            while (i < str.length() && sb[sb.length() - 1] == '*' && str[i] == '*')
+                i++;
+            if (i < str.length())
+                sb+=str[i];
             i++;
-        if (i < str.length())
-            sb+=str[i];
-        i++;
+        }
+
+        return sb;
     }
 
-    return sb;
-}
 
+    bool recc(string &s, string &t, int n, int m){
+        if(n==0 or m==0){
+            if(n==0 and m==0) return dp[n][m] = 1;
+            else if(m==1 and t[m-1] == '*') return dp[n][m] = 1;
+            else return dp[n][m] = 0;
+        }
 
-bool recc(string &s, string &t, int n, int m){
-    if(n==0 or m==0){
-        if(n==0 and m==0) return dp[n][m] = 1;
-        else if(m==1 and t[m-1] == '*') return dp[n][m] = 1;
-        else return dp[n][m] = 0;
-    }
+        if(dp[n][m] != -1) return dp[n][m];
+        bool flag = false;
 
-    if(dp[n][m] != -1) return dp[n][m];
-    bool flag = false;
-
-    if(t[m-1] == '*'){
-        flag = flag or recc(s, t, n-1, m)  or recc(s, t, n, m-1);
-    }
-    else if(t[m-1] == '?'){
-        flag = recc(s, t, n-1, m-1);
-    }
-    else{
-        if(s[n-1] == t[m-1]){
+        if(t[m-1] == '*'){
+            flag = flag or recc(s, t, n-1, m) or recc(s, t, n-1, m-1) or recc(s, t, n, m-1);
+        }
+        else if(t[m-1] == '?'){
             flag = recc(s, t, n-1, m-1);
         }
         else{
-            flag = false;
+            if(s[n-1] == t[m-1]){
+                flag = recc(s, t, n-1, m-1);
+            }
+            else{
+                flag = false;
+            }
         }
+        return dp[n][m] = flag;
     }
-    return dp[n][m] = flag;
-}
 
-bool isMatch(string s, string p) {
-    p = removeStars(p);
-    int n = s.size();
-    int m = p.size();
-    for(int i=0;i<=n;i++){
-        for(int j=0;j<=m;j++){
-            dp[i][j] = -1;
+    bool isMatch(string s, string p) {
+        p = removeStars(p);
+        int n = s.size();
+        int m = p.size();
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                dp[i][j] = -1;
+            }
         }
+        return recc(s, p, s.size(), p.size());
     }
-    return recc(s, p, s.size(), p.size());
-}
 };
