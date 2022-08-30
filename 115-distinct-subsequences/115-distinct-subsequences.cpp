a0971138a657dfd774c94c4584272d9498f39e87
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-30 20:48:23 +0530

    Time: 53 ms (62.69%), Space: 15.2 MB (70.03%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,21 +1,28 @@
 class Solution {
 private:
     const static int N = 1e3+1;
-    int dp[N][N];
-    long recc(string s, string t, int n, int m){
-        if(n<m) return dp[n][m] = 0;
-        if(m==0) return dp[n][m] = 1;
-        if(dp[n][m] != -1) return dp[n][m];
-        long cnt;
-        if(s[n-1] == t[m-1]){
-            cnt = (recc(s,t,n-1,m-1)+recc(s,t,n-1,m));
+    long long dp[N][N];
+long long tabu(string s, string t, int N, int M){
+    for(int n=0; n<=N; n++){
+        for(int m=0; m<=M; m++){
+            if(n<m){
+                dp[n][m] = 0;
+                continue;
+            }
+            if(m==0){
+                dp[n][m] = 1;
+                continue;
+            }
+            if(s[n-1] == t[m-1]){
+                dp[n][m] =  (dp[n-1][m-1]+dp[n-1][m])% 1000000007;
+            }
+            else{
+                dp[n][m] = dp[n-1][m];
+            }
         }
-        else{
-            cnt = (recc(s,t,n-1,m));
-        }
-
-        return dp[n][m] = cnt;
     }
+    return dp[N][M];
+}
 public:
     int numDistinct(string s, string t) {
         int n = s.size();
@@ -25,6 +32,6 @@ public:
                 dp[i][j] = -1;
             }
         }
-        return recc(s,t,n,m);
+        return tabu(s,t,n,m);
     }
 };
