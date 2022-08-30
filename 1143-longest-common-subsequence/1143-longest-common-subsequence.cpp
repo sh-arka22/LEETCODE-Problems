43316fd7e1fb93cc6cc5e96bb4654894e51b2361
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-30 08:42:56 +0530

    Time: 36 ms (51.90%), Space: 10.8 MB (67.65%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     const static int N = 1e3+3;
     int dp[N][N];
-    int recc(string &s1, string &s2, int n, int m){
-    if(n == 0 or m == 0){
-        return dp[n][m] = 0;
+    int tabu(string s1, string s2, int N, int M){
+    for(int n=0; n<=N; n++){
+        for(int m=0; m<=M; m++){
+            if(n == 0 or m == 0){
+                dp[n][m] = 0;
+                continue;
+            }
+            if(s1[n-1] == s2[m-1]){
+                dp[n][m] = dp[n-1][m-1]+1;
+            }
+            else {
+                dp[n][m] = max(dp[n-1][m], dp[n][m-1]);
+            }
+        }
     }
-    if(dp[n][m] != -1) return dp[n][m];
-    int cnt;
-    if(s1[n-1] == s2[m-1]){
-        cnt = recc(s1, s2, n-1, m-1)+1;
-    }
-    else {
-        cnt = max(recc(s1, s2, n-1, m), recc(s1, s2, n, m-1));
-    }
-    return dp[n][m] = cnt;
+    return dp[N][M];
 }
 
     int longestCommonSubsequence(string text1, string text2) {
@@ -24,6 +27,6 @@ public:
                 dp[i][j] = -1;
             }
         }
-        return recc(text1, text2, n,m);
+        return tabu(text1, text2, n,m);
     }
 };
