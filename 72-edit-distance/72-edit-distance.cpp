72973970efb322634d52c4ff8a4aaecf5b8e43ae
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-31 20:14:52 +0530

    Time: 526 ms (5.00%), Space: 15.1 MB (17.04%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -3,25 +3,25 @@ public:
     const static int N = 1e3+1;
     long dp[N][N];
     int a = 1, b = 1, c = 1; // if i was given insertin deltion and insertion cost
-int recc(string &s1, string &s2, int n, int m){
+    int recc(string &s1, string &s2, int n, int m){
 
-    if(n==0 or m==0){
-        return dp[n][m] = (n==0 ? m *a  : n*b);
+        if(n==0 or m==0){
+            return dp[n][m] = (n==0 ? m *a  : n*b);
+        }
+
+        if(dp[n][m] != -1) return dp[n][m];
+
+        int cnt;
+        if(s1[n-1]==s2[m-1]){
+            cnt = recc(s1, s2, n-1, m-1);
+        }
+        else{
+            cnt = min(recc(s1,s2,n,m-1)+a,(min(recc(s1,s2,n-1,m)+b, recc(s1,s2,n-1,m-1)+c)));
+            //        insert               delete                   replace
+            // cnt+=1;
+        }
+        return dp[n][m] = cnt;
     }
-    
-    if(dp[n][m] != -1) return dp[n][m];
-    
-    int cnt;
-    if(s1[n-1]==s2[m-1]){
-        cnt = recc(s1, s2, n-1, m-1);
-    }
-    else{
-        cnt = min(recc(s1,s2,n,m-1)+a,(min(recc(s1,s2,n-1,m)+b, recc(s1,s2,n-1,m-1)+c)));
-        //        insert               delete                   replace
-        // cnt+=1;
-    }
-    return dp[n][m] = cnt;
-}
 
 
     int minDistance(string word1, string word2) {
