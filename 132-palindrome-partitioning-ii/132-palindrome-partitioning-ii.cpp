a2944921ab192f7ce2a12c208b361ee807fb7506
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-01 04:08:06 +0530

    Time: 411 ms (60.75%), Space: 6.7 MB (70.61%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int *dp;
+bool isPalindrome(int i, int j, string &s){
+    while(i<j){
+        if(s[i] != s[j]) return false;
+        i++;j--;
+    }
+    return true;
+}
+
+int recc(int i,int n,string &s){
+    if(i==n) return dp[i] = 0;
+    if(dp[i] != -1) return dp[i];
+    int mini = INT_MAX;
+    
+    for(int j=i;j<n;j++){
+        if(isPalindrome(i, j, s)){
+            int cnt = 1+recc(j+1, n, s);
+            mini = min(mini,cnt);
+        }
+    }
+    return dp[i] = mini;
+}
+
+int minCut(string s) {
+    int n = s.size();
+    dp = new int[n+1];
+    fill(dp,dp+n+1,-1);
+    return recc(0, n, s)-1;
+}
+};
