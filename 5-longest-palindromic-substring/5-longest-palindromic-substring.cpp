256b4e68fec0c1d022994c32b129df91de3dc971
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-08-31 23:44:23 +0530

    Time: 420 ms (30.32%), Space: 23.7 MB (40.30%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,8 +1,7 @@
 class Solution {
-public:
-    string longestPalindrome(string s) {
+private:
+    string method01(string &s){
         int n = s.size();
-        
         int len = 0;
         string res;
         for(int i=0;i<n;i++){
@@ -30,5 +29,58 @@ public:
             }
         }
         return res;
+    }
+    
+    
+    string method02(string &s){
+        int n = s.size();
+        int dp[n][n];
+        for(int i=0; i<n; i++) for(int j=0; j<n;j++) dp[i][j] = 0;
+        string res = "";
+        int maxi = 0;
+        for(int gap = 1; gap<=n; gap++){
+           for(int i=0, j = gap-1; j<n; i++,j++) {
+
+                if(s[i]==s[j]){
+                    if(gap == 1){
+                        dp[i][j] = 1;
+                        res = s.substr(i,gap);
+                        maxi = 1;
+                        continue;
+                    }
+                    if(gap == 2){
+                        dp[i][j] = 2;
+                        res = s.substr(i,gap);
+                        maxi = 2;
+                        continue;
+                    }
+                    if(dp[i+1][j-1]){
+                        dp[i][j] = gap;
+                        if(gap>maxi){
+                            res = s.substr(i,gap);
+                            maxi = gap;
+                        }
+                    }
+                    else{
+                        dp[i][j] = 0;
+                    }
+                }
+                else{
+                    dp[i][j] = 0;
+                }
+           }
+        }
+
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++) {
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        return res;
+    }
+public:
+    string longestPalindrome(string s) {
+        return method02(s);
     }
 };
