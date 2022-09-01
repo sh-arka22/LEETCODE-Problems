341f247ef4f829abdd64c18366b577390cefe115
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-01 06:03:36 +0530

    Time: 175 ms (90.32%), Space: 48.6 MB (99.77%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.cpp
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -1,52 +1,41 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        
-        vector<int> graph[n];
+        vector<int>graph[n];
         int indegree[n];
-        fill(indegree, indegree+n,0);
-        for(int i=0;i<n-1;i++){
-            int v = edges[i][0];
-            int u = edges[i][1];
+        fill(indegree, indegree+n, 0);
+        
+        for(auto &edge:edges){
+            int v = edge[0], u = edge[1];
             graph[v].push_back(u);
             graph[u].push_back(v);
-            indegree[v]++, indegree[u]++;
+            indegree[u]++, indegree[v]++;
         }
         
         queue<int>que;
+        for(int i=0;i<n;i++) if(indegree[i] == 1) que.push(i);
         
-        for(int i=0;i<n;i++){
-            if(indegree[i] == 1){
-                que.push(i);
-            }
-        }
-        
-        int rad = 0;
         while(que.size()){
             int sz = que.size();
-            if(n == 2){
-                vector<int>ans;
-                ans.push_back(que.front());
+            if(n==1){
+                    return {que.front()};
+                }
+                if(n==2){
+                    int first = que.front(); que.pop();
+                    int second = que.front();
+                    return {first,second};
+                }
+            for(int i=0;i<sz;i++){
+                int parr = que.front();
                 que.pop();
-                ans.push_back(que.front());
-                return ans;
-            }
-            if(n == 1){
-                return {que.front()};
-            }
-            while(sz--){
-                int leaf = que.front();
-                que.pop();
-                n--; // one leaf node got processed
-                for(auto child : graph[leaf]){
-                    if(--indegree[child] == 1){
+                n--;
+                
+                for(auto child:graph[parr]){
+                    if(--indegree[child]==1)
                         que.push(child);
-                    }
                 }
             }
-            rad++;
         }
         return {0};
     }
-        
 };
