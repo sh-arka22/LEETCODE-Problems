7286250fe0263188dd8620b09aaaeaa3b2843761
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-03 00:40:03 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (66.88%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-uint *dp;
-int permutationTable(vector<int>nums, int Tar){
-	dp[0] = 1;
-	for(int tar = 1; tar<=Tar; tar++){
-		uint cnt = 0;
-		for(auto ele:nums){
-			if(tar - ele >= 0){
-				cnt += dp[tar-ele];
-			}
-		}
-		dp[tar] = cnt;
-	}
-	return dp[Tar];
-}
+    uint *dp;
+    int permutationTable(vector<int>nums, int Tar){
+        dp[0] = 1;
+        for(int tar = 1; tar<=Tar; tar++){
+            uint cnt = 0;
+            for(auto ele:nums){
+                if(tar - ele >= 0){
+                    cnt += dp[tar-ele];
+                }
+            }
+            dp[tar] = cnt;
+        }
+        return dp[Tar];
+    }
 
-int combinationSum4(vector<int>& nums, int target) {
-    dp = new uint[target+1];
-    fill(dp, dp+target+1,0);
-    return permutationTable(nums, target);        
-}
+    int combinationSum4(vector<int>& nums, int target) {
+        dp = new uint[target+1];
+        fill(dp, dp+target+1,0);
+        return permutationTable(nums, target);        
+    }
 };
