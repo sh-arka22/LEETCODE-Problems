029a3e6b2ec145546d7500883355a46de458c97f
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-02 05:43:09 +0530

    Time: 587 ms (24.46%), Space: 10.6 MB (44.60%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int *dp;
-
-    int recc(vector<int>&nums, int ei){
-        if(ei == 0) return dp[ei] = 1;
-        if(dp[ei] != 0) return dp[ei];
-        int maxLen = 1;
-        for(int i = ei-1;i>=0;i--){
-            if(nums[i] >= nums[ei]) continue;
-            int faith = recc(nums, i);
-            maxLen = max(maxLen, faith+1);
-        }
-        return dp[ei] = maxLen;
-    }
-
-    int lengthOfLIS(vector<int>&nums) {
-        dp = new int[nums.size()];
-        fill(dp,dp+nums.size(),0);
-        int maxLen = 0;
-        for(int i=0;i<nums.size();i++){
-            maxLen = max(maxLen, recc(nums, i));
+    int tabu(vector<int>& nums){
+        int n = nums.size(), maxLen = 0;
+        dp = new int[n];
+        for(int i=0;i<n;i++){
+            dp[i] = 1;
+            for(int j=i-1;j>=0;j--){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[i], dp[j] + 1); 
+                }
+            }
+            maxLen = max(maxLen,dp[i]);
         }
         return maxLen;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        return tabu(nums);
     }
 };
