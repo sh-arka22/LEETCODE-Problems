2b943b3513e179c8254f4e0fb7e116d3b0dc8bf2
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-03 20:08:13 +0530

    Time: 2586 ms (5.01%), Space: 8.8 MB (98.79%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int recc(vector<int> &nums, int n, int tar){
+        if(n==0) return tar==0;
+
+        int cnt = 0;
+
+        cnt += recc(nums,n-1, tar - (nums[n-1]));
+        cnt += recc(nums,n-1, tar - (-nums[n-1]));
+
+        return cnt;
+    }
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = accumulate(begin(nums), end(nums), 0);
+        if(sum < target or -sum > target) return 0;
+
+        return recc(nums,nums.size(), target);
+    }
+};
