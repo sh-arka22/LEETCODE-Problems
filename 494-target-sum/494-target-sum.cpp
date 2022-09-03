7a387acc85e27aa6a5d085a1fac74329b3c8924f
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-03 22:47:35 +0530

    Time: 55 ms (46.87%), Space: 22.6 MB (24.75%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     vector<vector<int>>dp;
 // map<pair<int,int>,int>dp;
-int recc(vector<int> &nums, int n, int tar, int sum){
-    if(n==0) return dp[n][sum] = (tar==sum);
-    if(dp[n][sum] != -1) return dp[n][sum];
-    int cnt = 0;
+    int recc(vector<int> &nums, int n, int tar, int sum){
+        if(n==0) return dp[n][sum] = (tar==sum);
+        if(dp[n][sum] != -1) return dp[n][sum];
+        int cnt = 0;
 
-    cnt += recc(nums,n-1, tar , sum + (nums[n-1]));
-    cnt += recc(nums,n-1, tar , sum - (nums[n-1]));
+        cnt += recc(nums,n-1, tar , sum + (nums[n-1]));
+        cnt += recc(nums,n-1, tar , sum - (nums[n-1]));
 
-    return dp[n][sum] = cnt;
-}
+        return dp[n][sum] = cnt;
+    }
 
-int findTargetSumWays(vector<int>& nums, int target) {
-    int sum = accumulate(begin(nums), end(nums), 0);
-    if(sum < target or sum < -target) return 0;
-    dp.resize(nums.size()+1, vector<int>(2*sum+1, -1));
-    return recc(nums,nums.size(), sum + target, sum);
-}
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = accumulate(begin(nums), end(nums), 0);
+        if(sum < target or sum < -target) return 0;
+        dp.resize(nums.size()+1, vector<int>(2*sum+1, -1));
+        return recc(nums,nums.size(), sum + target, sum);
+    }
 };
