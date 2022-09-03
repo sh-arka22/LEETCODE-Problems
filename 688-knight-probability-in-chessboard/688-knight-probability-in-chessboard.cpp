98bc835930dff1183fffd0616e2196e900da3b53
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-04 00:16:30 +0530

    Time: 16 ms (75.47%), Space: 9.4 MB (23.36%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.cpp
@@ -1,23 +1,24 @@
 class Solution {
-private:
-    int dirs[8][2] = {{-2,1}, {-1,2}, {1,2}, {2,1}, {2,-1}, {1,-2}, {-1,-2}, {-2,-1}};
 public:
-    double knightProbability(int N, int K, int r, int c) {
-        vector<vector<double>> m(N, vector<double>(N, 1));
-        while (K-- > 0) {
-            vector<vector<double>> next(N, vector<double>(N, 0));
-            for (int i = 0; i < N; ++i) {
-                for (int j = 0; j < N; ++j) {
-                    for (auto &dir : dirs) {
-                        int x = i + dir[0], y = j + dir[1];
-                        if (x < 0 || x >= N || y < 0 || y >= N) continue;
-                        next[i][j] += m[x][y];
-                    }
-                    next[i][j] /= 8;
-                }
-            }
-            m = next;
+    int dirs[8][2] = {{-2,1}, {-1,2}, {1,2}, {2,1}, {2,-1}, {1,-2}, {-1,-2}, {-2,-1}};
+    vector<vector<vector<double>>>dp;
+    double recc(int n, int k, int row, int col){
+        if(k==0) return dp[k][row][col] = 1.0;
+        if(dp[k][row][col] != 0.0) return dp[k][row][col];
+        double cnt = 0;
+        for(auto &[dr,dc]: dirs){
+            int r = row+dr, c = col+dc;
+            if(r>=0 and c>=0 and r<n and c<n)
+                cnt += recc(n, k-1, r, c);
         }
-        return m[r][c];
+        return dp[k][row][col] = cnt/8.0;
+    }
+
+
+
+
+    double knightProbability(int n, int k, int row, int column) {
+        dp.resize(k+1, vector<vector<double>>(n+1, vector<double>(n+1, 0.0)));
+        return recc(n, k, row, column);
     }
 };
