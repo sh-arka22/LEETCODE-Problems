749e003be00a082603e6a2c7242ef970f3a7c690
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-04 23:22:53 +0530

    Time: 43 ms (23.77%), Space: 17.9 MB (23.36%) - LeetHub

---
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        
+        
+        int n = nums.size();
+        vector<int> res(n,0);
+        int i=1;
+        int j = n-1;
+        while(i<n){
+            res[i] = nums[j];
+            i+=2;
+            j--;
+        }
+        
+        i=0;
+        while(j>=0){
+            res[i] = nums[j];
+            i+=2;
+            j--;
+        }
+        nums.assign(res.begin(), res.end());
+        // copy(res.begin(), res.end(), back_inserter(nums));
+    }
+};
