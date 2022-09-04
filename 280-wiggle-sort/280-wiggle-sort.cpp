f9b83ecce84cd84891aaae08dc03ef7f03228ae3
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-04 22:52:34 +0530

    Time: 25 ms (18.88%), Space: 13.7 MB (23.30%) - LeetHub

---
+++ 280-wiggle-sort/280-wiggle-sort.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        int n = nums.size();
+        
+        for(int i=0;i<n-1;i++){
+            if(i%2 == 0){
+                //odd
+                if(nums[i]>nums[i+1])
+                    swap(nums[i],nums[i+1]);
+            }
+            else{
+                if(nums[i+1]>nums[i])
+                    swap(nums[i], nums[i+1]);
+            }
+        }
+    }
+};
