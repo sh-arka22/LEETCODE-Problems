4988999f190634628dd99377e7fa629717232b16
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-05 11:51:37 +0530

    Time: 98 ms (83.98%), Space: 31.4 MB (83.43%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -11,14 +11,14 @@ public:
             int idx = abs(nums[i]);
             if(idx>0 and idx<=n){
                 if(nums[idx-1] == 0){
-                    nums[idx-1] = -1*n;
+                    nums[idx-1] = INT_MIN;
                 }
                 else if(nums[idx-1]>0)
                     nums[idx-1] *= -1;
             }
         }
         
-        for(int i=1;i<n+1;i++){
+        for(int i=1;i<=n;i++){
             if(nums[i-1]>=0){
                 return i;
             }
