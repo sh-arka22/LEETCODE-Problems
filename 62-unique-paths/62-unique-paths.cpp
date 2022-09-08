583b585f0a494e69d823593db38bf89fa70fdc5c
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-08 07:33:48 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.55%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+vector<vector<int>>dp;
+int recc(int n, int m){
+    if(n==0 or m==0) return dp[n][m] = 1;
+    if(dp[n][m] != -1) return dp[n][m];
+    int left = recc(n-1,m);
+    int right = recc(n,m-1);
+
+    return dp[n][m] = (left + right);
+}
+
+int uniquePaths(int m, int n) {
+    dp.resize(m, vector<int>(n,-1));
+    return recc(m-1,n-1);
+}
+};
