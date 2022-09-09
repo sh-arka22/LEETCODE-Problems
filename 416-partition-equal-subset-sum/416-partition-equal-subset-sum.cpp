66be34916666001b18fbf25c4e35c38b23919485
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-09 11:40:01 +0530

    Time: 930 ms (7.19%), Space: 99.2 MB (13.35%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,24 +1,32 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    bool recc(int n, int tar, vector<int>arr){
-        if(n==0 or tar ==arr[n]) return dp[tar][n] = (tar==arr[n]);
-        if(dp[tar][n] != -1) return dp[tar][n];
-        bool flag = false;
-        if(tar - arr[n] >= 0){
-            flag = flag or recc(n-1, tar-arr[n], arr);
+bool tabu(int N, int Tar, vector<int>&arr){
+    for(int n=0; n<=N;n++){
+        for(int tar=0;tar<=Tar;tar++){
+            if(n==0 or tar ==arr[n]){
+                dp[tar][n] = (tar==arr[n]);
+                continue;
+            }
+            bool flag = false;
+            if(tar - arr[n] >= 0){
+                flag = flag or dp[tar-arr[n]][n-1];
+            }
+            flag = flag or dp[tar][n-1];
+
+            dp[tar][n] = flag;
         }
-        flag = flag or recc(n-1, tar, arr);
-
-        return dp[tar][n] = flag;
     }
+    return dp[Tar][N];
+}
 
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum%2) return false;
-        dp.resize((sum/2)+1,vector<int>(n,-1));
-        int ans = recc(n-1, sum/2, nums);
-        return ans;   
-    }
+bool canPartition(vector<int>& nums) {
+    int n = nums.size();
+    int sum = accumulate(nums.begin(), nums.end(), 0);
+    if(sum%2) return false;
+    dp.resize((sum/2)+1,vector<int>(n,0));
+    int ans = tabu(n-1, sum/2, nums);
+    dp.clear();
+    return ans ;   
+}
 };
