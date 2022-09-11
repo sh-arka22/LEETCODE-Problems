9fa8432be6840b20a1bd249f4e61a625bc232242
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-11 12:42:43 +0530

    Time: 29 ms (57.70%), Space: 12.3 MB (38.80%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,22 +1,43 @@
 class Solution {
-public:
-    vector<vector<int>>dp;
-// map<pair<int,int>,int>dp;
-    int recc(vector<int> &nums, int n, int tar, int sum){
-        if(n==0) return dp[n][sum] = (tar==sum);
-        if(dp[n][sum] != -1) return dp[n][sum];
-        int cnt = 0;
-
-        cnt += recc(nums,n-1, tar , sum + (nums[n-1]));
-        cnt += recc(nums,n-1, tar , sum - (nums[n-1]));
-
-        return dp[n][sum] = cnt;
+private:
+int findWaysUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+    if(ind == 0){
+        if(target == 0 and arr[0] == 0) return 2; // arr->{0}={},{0}
+        if(target == 0 or target == arr[0]) return 1;
+        return 0;
     }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = findWaysUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= (notTaken + taken)%1000000007;
+}
 
+int countPartitions(int n, int d, vector<int> &arr) {
+    // Write your code here.
+    int total = accumulate(begin(arr), end(arr), 0);
+    if(d>total) return 0;
+    int s1 = (d+total);
+    if(s1%2) return 0;
+    s1 = s1 >> 1;
+    vector<vector<int>> dp(n,vector<int>(abs(s1+1),-1));
+    return findWaysUtil(n-1,s1,arr,dp);
+}
+
+
+int targetSum(int n, int target, vector<int>& arr) {
+    // Write your code here.
+    return countPartitions(n, target, arr);
+}
+public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = accumulate(begin(nums), end(nums), 0);
-        if(sum < target or sum < -target) return 0;
-        dp.resize(nums.size()+1, vector<int>(2*sum+1, -1));
-        return recc(nums,nums.size(), sum + target, sum);
+        int n = nums.size();
+        return targetSum(n, target, nums);
     }
 };
