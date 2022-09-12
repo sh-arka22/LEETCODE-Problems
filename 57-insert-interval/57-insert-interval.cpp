6d3e2875f04ab5ce9a042d304b60e29de5552ab9
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-12 12:19:15 +0530

    Time: 83 ms (5.13%), Space: 17.5 MB (17.63%) - LeetHub

--- 57-insert-interval/57-insert-interval.cpp
+++ 57-insert-interval/57-insert-interval.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
         vector<vector<int>>ans;
-        int n = intervals.size();
-        if(n == 0){
-            vector<vector<int>>tmp;
-            tmp.push_back(newInterval);
-            return tmp;
-        }
-        int pos = 0;
+        sort(intervals.begin(), intervals.end());
         for(auto interval:intervals){
-            if(interval[0]>=newInterval[0]){
-                break;
+            if(!ans.size()){
+                ans.push_back(interval);
             }
-            ans.push_back(interval);
-            pos++;
+            else{
+                if(ans.back()[1] >= interval[0]){
+                    ans.back()[1] = max(ans.back()[1], interval[1]);
+                }
+                else ans.push_back(interval);
+            }
         }
-        if(ans.size() == 0 or ans.back()[1] < newInterval[0]){
-            ans.push_back(newInterval);
-        }
-        else{
-            ans.back()[1] = max(ans.back()[1], newInterval[1]);
-        }
-        
-        while(pos<n){
-            if(intervals[pos][0] <= ans.back()[1])
-                ans.back()[1] = max(intervals[pos++][1], ans.back()[1]);
-            else ans.push_back(intervals[pos++]);
-        }
-        
         return ans;
     }
 };
