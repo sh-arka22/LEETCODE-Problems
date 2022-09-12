8b39c50c227d1e94d3e78bd621787b11668f0176
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-12 05:40:30 +0530

    Time: 56 ms (21.62%), Space: 13.1 MB (44.72%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,32 +1,33 @@
 class Solution {
-public:
-    const static int N = 1e3+3;
-    int dp[N][N];
-    int tabu(string s1, string s2, int N, int M){
-    for(int n=0; n<=N; n++){
-        for(int m=0; m<=M; m++){
-            if(n == 0 or m == 0){
-                dp[n][m] = 0;
-                continue;
-            }
-            if(s1[n-1] == s2[m-1]){
-                dp[n][m] = dp[n-1][m-1]+1;
-            }
-            else {
-                dp[n][m] = max(dp[n-1][m], dp[n][m-1]);
+private:
+    vector<vector<int>>dp;
+    int recc(string s, int N, string t, int M){
+        for(int n=0;n<=N; n++){
+            for(int m=0; m<=M; m++){
+                if(n==0 or m==0){
+                    dp[n][m] = 0;
+                    continue;
+                }
+                int len = 0;
+                if(s[n-1] == t[m-1]){
+                    len = dp[n-1][m-1]+ 1;
+                }
+                else len = max(dp[n-1][m], dp[n][m-1]);
+                dp[n][m] = len;
             }
         }
+        return dp[N][M];
     }
-    return dp[N][M];
-}
 
+    int lcs(string s, string t){
+        int n = s.size(), m = t.size();
+        dp.resize(n+1, vector<int>(m+1,0));
+        int ans = recc(s, n, t, m);
+        dp.clear();
+        return ans;
+    }
+public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size(), m = text2.size();
-        for(int i = 0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                dp[i][j] = -1;
-            }
-        }
-        return tabu(text1, text2, n,m);
+        return lcs(text1, text2);
     }
 };
