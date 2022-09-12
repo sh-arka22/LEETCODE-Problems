acb078801da5155c0897715d01b09e1ae8d3a29c
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-12 16:12:09 +0530

    Time: 85 ms (51.96%), Space: 27.1 MB (47.64%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int recc(string &s, int st, int en, vector<vector<int>>&dp){
+        if(st>=en) return dp[st][en] = (st == en ? 1 : 0);
+        if(dp[st][en] != -1) return dp[st][en];
+        int cnt;
+        if(s[st] != s[en]){
+            cnt = max(recc(s,st+1,en,dp),recc(s,st,en-1,dp));
+        }
+        else cnt = (recc(s,st+1,en-1,dp)+2);
+
+        return dp[st][en] = cnt;
+    }
+
+    int LPS(string &s) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
+        return recc(s,0,s.size()-1,dp);
+    }
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+        return n - LPS(s);
+    }
+};
