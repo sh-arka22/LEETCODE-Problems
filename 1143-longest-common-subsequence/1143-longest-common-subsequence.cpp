6a14a21e8e6bebcd04eab1790f0ab9d4840bfb6a
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-12 05:49:44 +0530

    Time: 17 ms (85.61%), Space: 13.2 MB (12.20%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,27 +1,31 @@
 class Solution {
 private:
     vector<vector<int>>dp;
+    vector<int>prev, curr;
     int recc(string s, int N, string t, int M){
         for(int n=0;n<=N; n++){
             for(int m=0; m<=M; m++){
                 if(n==0 or m==0){
-                    dp[n][m] = 0;
+                    curr[m] = 0;
                     continue;
                 }
                 int len = 0;
                 if(s[n-1] == t[m-1]){
-                    len = dp[n-1][m-1]+ 1;
+                    len = prev[m-1]+ 1;
                 }
-                else len = max(dp[n-1][m], dp[n][m-1]);
-                dp[n][m] = len;
+                else len = max(prev[m], curr[m-1]);
+                curr[m] = len;
             }
+            prev = curr;
         }
-        return dp[N][M];
+        return curr[M];
     }
 
     int lcs(string s, string t){
         int n = s.size(), m = t.size();
         dp.resize(n+1, vector<int>(m+1,0));
+        prev.resize(m+1, 0);
+        curr.resize(m+1, 0);
         int ans = recc(s, n, t, m);
         dp.clear();
         return ans;
