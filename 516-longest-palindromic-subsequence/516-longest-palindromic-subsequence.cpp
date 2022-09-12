fea2377c5b8a910d926346ef42fe4f9370e7d4aa
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-12 15:07:29 +0530

    Time: 131 ms (72.41%), Space: 73.2 MB (9.82%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-int tabu(string s, int ST, int EN,vector<vector<int>>&dp){
-    for(int gap=0; gap<s.size();gap++){
-        for(int st=0, en = gap; en<s.size();st++,en++){
-            if(st>=en) {
-                dp[st][en] = st == en ? 1 : 0;
-                continue;
-            }
-            if(s[st] != s[en]){
-                dp[st][en] = max(dp[st+1][en],dp[st][en-1]);
-            }
-            else dp[st][en] = (dp[st+1][en-1]+2);    
+    int recc(string &s, int st, int en, vector<vector<int>>&dp){
+        if(st>=en) return dp[st][en] = (st == en ? 1 : 0);
+        if(dp[st][en] != 0) return dp[st][en];
+        int cnt;
+        if(s[st] != s[en]){
+            cnt = max(recc(s,st+1,en,dp),recc(s,st,en-1,dp));
         }
+        else cnt = (recc(s,st+1,en-1,dp)+2);
+
+        return dp[st][en] = cnt;
     }
-    return dp[ST][EN];
-}
 
 int longestPalindromeSubseq(string s) {
     vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,0));
-    return tabu(s,0,s.size()-1,dp);
-}
+        return recc(s,0,s.size()-1,dp);
+    }
 };
