86154d100a13a942e2eb304a0243e7d9bbfb2b05
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-13 20:54:33 +0530

    Time: 63 ms (48.49%), Space: 30.2 MB (16.91%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,37 +1,34 @@
 class Solution {
-private:
-    const static int N = 1e3+1;
-    long long dp[N][N];
-long long tabu(string s, string t, int N, int M){
-    for(int n=0; n<=N; n++){
-        for(int m=0; m<=M; m++){
-            if(n<m){
-                dp[n][m] = 0;
-                continue;
-            }
-            if(m==0){
-                dp[n][m] = 1;
-                continue;
-            }
-            if(s[n-1] == t[m-1]){
-                dp[n][m] =  (dp[n-1][m-1]+dp[n-1][m])% 1000000007;
-            }
-            else{
-                dp[n][m] = dp[n-1][m];
-            }
-        }
-    }
-    return dp[N][M];
-}
 public:
-    int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                dp[i][j] = -1;
+    vector<vector<long long>>dp;
+    int recc(string &s, string &t, int N, int M){
+        for(int n=0;n<=N;n++){
+            for(int m=0;m<=M;m++){
+                if(n==0 or m==0){
+                    if(m==0){
+                        dp[n][m] = 1;
+                        continue;
+                    }
+                    if(n==0){
+                        dp[n][m] = 0;
+                        continue;
+                    } 
+                }
+                int left = 0, right = 0;
+                if(s[n-1] == t[m-1]){
+                    left = dp[n-1][m-1];
+                }
+                right = dp[n-1][m];
+                dp[n][m] = (left+right)%1000000007;
             }
         }
-        return tabu(s,t,n,m);
+        return dp[N][M];
+    }
+
+
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        dp.resize(n+1,vector<long long>(m+1,0));
+        return recc(s, t, n, m);
     }
 };
