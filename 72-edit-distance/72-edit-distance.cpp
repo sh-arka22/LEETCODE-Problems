bb3a0bfd981355591e5984070ac53b1a97dc1834
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-13 23:53:07 +0530

    Time: 154 ms (12.31%), Space: 82 MB (5.80%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,38 +1,31 @@
 class Solution {
-public:
-    const static int N = 1e3+1;
-    long dp[N][N];
-    int a = 1, b = 1, c = 1; // if i was given insertin deltion and insertion cost
-    int recc(string &s1, string &s2, int n, int m){
-
-        if(n==0 or m==0){
-            return dp[n][m] = (n==0 ? m *a  : n*b);
-        }
-
-        if(dp[n][m] != -1) return dp[n][m];
-
-        int cnt;
-        if(s1[n-1]==s2[m-1]){
-            cnt = recc(s1, s2, n-1, m-1);
-        }
-        else{
-            cnt = min(recc(s1,s2,n,m-1)+a,(min(recc(s1,s2,n-1,m)+b, recc(s1,s2,n-1,m-1)+c)));
-            //        insert               delete                   replace
-            // cnt+=1;
-        }
-        return dp[n][m] = cnt;
+private:
+    vector<vector<int>>dp;
+int recc(string s1, string s2, int n, int m){
+    if(n==0 or m==0){
+        if(n>0) return dp[n][m] = n;
+        if(m>0) return dp[n][m] = m;
+        return dp[n][m] = 0;
+    }
+    
+    if(dp[n][m] != -1) return dp[n][m];
+    int deletion = (int)1e9, insertion = (int)1e9, replace = (int)1e9;
+    
+    if(s1[n-1] == s2[m-1]){
+        replace = recc(s1, s2, n-1, m-1);
+    }
+    else{
+        deletion = recc(s1, s2, n-1, m)+1;
+        insertion = recc(s1, s2, n, m-1)+1;
+        replace = recc(s1, s2, n-1, m-1)+1;
     }
 
-
+    return dp[n][m] = min(insertion, min(deletion,replace));
+}
+public:
     int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                dp[i][j] = -1;
-            }
-        }
-        int ans = recc(word1,word2,n,m);
-        return ans;
+        int n = word1.size(), m = word2.size();
+        dp.resize(n+1, vector<int>(m+1,-1));
+        return recc(word1, word2, n, m);
     }
 };
