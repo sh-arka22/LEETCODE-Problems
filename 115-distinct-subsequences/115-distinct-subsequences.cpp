de51ecd5314917c72982833440de8188490e634e
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-13 21:03:55 +0530

    Time: 60 ms (52.70%), Space: 6.4 MB (96.19%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-    vector<vector<long long>>dp;
+    vector<int> curr, prev;
     int recc(string &s, string &t, int N, int M){
         for(int n=0;n<=N;n++){
             for(int m=0;m<=M;m++){
                 if(n==0 or m==0){
                     if(m==0){
-                        dp[n][m] = 1;
+                        curr[m] = 1;
                         continue;
                     }
                     if(n==0){
-                        dp[n][m] = 0;
+                        curr[m] = 0;
                         continue;
                     } 
                 }
                 int left = 0, right = 0;
                 if(s[n-1] == t[m-1]){
-                    left = dp[n-1][m-1];
+                    left = prev[m-1];
                 }
-                right = dp[n-1][m];
-                dp[n][m] = (left+right)%1000000007;
+                right = prev[m];
+                curr[m] = (left+right)%1000000007;
             }
+            prev = curr;
         }
-        return dp[N][M];
+        return curr[M];
     }
 
 
     int numDistinct(string s, string t) {
         int n = s.size(), m = t.size();
-        dp.resize(n+1,vector<long long>(m+1,0));
+        curr.resize(m+1, 0);
+        prev.resize(m+1, 0);
         return recc(s, t, n, m);
     }
 };
