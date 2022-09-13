3efb97541bb7f1be9e6253f5ea4ced65f91fd1b9
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-13 13:25:19 +0530

    Time: 44 ms (5.23%), Space: 15 MB (12.08%) - LeetHub

---
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.cpp
@@ -0,0 +1,28 @@
+bool comparator(string a, string b){
+        int i = a.find(' ');
+        int j = b.find(' ');
+        
+        if(!isdigit(a[i+1]) and !isdigit(b[j+1])){
+            if(a.substr(i+1) == b.substr(j+1))
+                return a<b;
+            return a.substr(i+1) < b.substr(j+1);
+        }
+        else if(isdigit(a[i+1]) and !isdigit(b[j+1])){
+            return false;
+        }
+        else if(!isdigit(a[i+1]) and isdigit(b[j+1])){
+            return true;
+        }
+        // else if(isdigit(a[i+1]) and isdigit(b[j+1])){
+        //     return true;
+        // }
+        
+        return false;
+    }
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        stable_sort(logs.begin(), logs.end(), comparator);
+        return logs;
+    }
+};
