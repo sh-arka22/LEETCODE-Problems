75988b93181be987f2018f6fc1c576ece4f238de
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-15 03:50:21 +0530

    Time: 1582 ms (7.77%), Space: 218.1 MB (10.14%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,35 +1,34 @@
 class Solution {
 private:
-int getAns(vector<int>& Arr, int n, int ind, int buy, int cap, vector<vector<vector<int>>>& dp ){
+    vector<vector<vector<int>>> dp;
+    int getAns(vector<int>& Arr, int n, int ind, int buy, int cap){
 
-    if(ind==n || cap==0) return 0; //base case
-    
-    if(dp[ind][buy][cap]!=-1)
-        return dp[ind][buy][cap];
-        
-    int profit;
-    
-    if(buy==0){// We can buy the stock
-        profit = max(0+getAns(Arr,n,ind+1,0,cap,dp), 
-                    -Arr[ind] + getAns(Arr,n,ind+1,1,cap,dp));
-    }
-    
-    if(buy==1){// We can sell the stock
-        profit = max(0+getAns(Arr,n,ind+1,1,cap,dp),
-                    Arr[ind] + getAns(Arr,n,ind+1,0,cap-1,dp));
-    }
-    
-    return dp[ind][buy][cap] = profit;
-}    
+        if(ind==n || cap==0) return dp[ind][buy][cap] = 0; //base case
+
+        if(dp[ind][buy][cap]!=-1)
+            return dp[ind][buy][cap];
+
+        int profit;
+
+        if(buy==0){// We can buy the stock
+            profit = max(0+getAns(Arr,n,ind+1,0,cap), 
+                        -Arr[ind] + getAns(Arr,n,ind+1,1,cap));
+        }
+
+        if(buy==1){// We can sell the stock
+            profit = max(0+getAns(Arr,n,ind+1,1,cap),
+                        Arr[ind] + getAns(Arr,n,ind+1,0,cap-1));
+        }
+
+        return dp[ind][buy][cap] = profit;
+    }    
 public:
-int maxProfit(vector<int>& prices)
-{   int n = prices.size();
-    // Creating a 3d - dp of size [n][2][3]
-    vector<vector<vector<int>>> dp(n,
-                                    vector<vector<int>> 
-                                            (2,vector<int>(3,-1)));
-    
-    return getAns(prices,n,0,0,2,dp);
-   
-}
+    int maxProfit(vector<int>& prices)
+    {   int n = prices.size();
+        // Creating a 3d - dp of size [n][2][3]
+        dp.resize(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+
+        return getAns(prices,n,0,0,2);
+
+    }
 };
