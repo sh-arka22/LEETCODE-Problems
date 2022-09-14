d9e06aace895b6fc1ec8a85cd22c5df94d8ed1a9
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-15 02:19:00 +0530

    Time: 22 ms (16.79%), Space: 17.3 MB (10.35%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,40 +1,24 @@
 class Solution {
 private:
-    long getAns(long *Arr, int ind, int buy, int n, vector<vector<long>> &dp ){
-
-        if(ind==n) return 0; //base case
-
-        if(dp[ind][buy]!=-1)
-            return dp[ind][buy];
-
-        long profit;
-
-        if(buy==0){// We can buy the stock
-            profit = max(0+getAns(Arr,ind+1,0,n,dp), getAns(Arr,ind+1,1,n,dp)-Arr[ind] );
+    vector<vector<int>>dp;
+    int recc(vector<int>& prices, int idx, int n, int state) {
+        if(idx == n) return dp[idx][state] = 0;
+        if(dp[idx][state] != -1) return dp[idx][state];
+        int profit;
+        if(state == 0){
+            //buy state;
+            profit = max(recc(prices, idx+1, n, 0), recc(prices, idx+1, n, 1) - prices[idx]);
         }
-
-        else if(buy==1){// We can sell the stock
-            profit = max(0+getAns(Arr,ind+1,1,n,dp), Arr[ind] + getAns(Arr,ind+1,0,n,dp));
+        else{
+            //sell state;
+            profit = max(recc(prices, idx+1, n, 1), prices[idx] + recc(prices, idx, n, 0));
         }
-
-        return dp[ind][buy] = profit;
-    }
-
-
-    long getMaximumProfit(long *Arr, int n) {
-        //Write your code here
-
-        vector<vector<long>> dp(n,vector<long>(2,-1));
-
-        if(n==0) return 0;
-        long ans = getAns(Arr,0,0,n,dp);
-        return ans;
+        return dp[idx][state] = profit;
     }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        long arr[n];
-        for(int i=0;i<n;i++) arr[i] = prices[i];
-        return getMaximumProfit(arr, n);
+        dp.resize(n+1, vector<int>(2,-1));
+        return recc(prices, 0, n , 0);
     }
 };
