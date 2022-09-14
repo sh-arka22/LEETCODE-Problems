23f4497b795866ad24865f49ee20fc538ee9e4bc
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-15 03:53:41 +0530

    Time: 1911 ms (5.01%), Space: 283.9 MB (5.05%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,10 +3,10 @@ private:
     vector<vector<vector<int>>> dp;
     int getAns(vector<int>& Arr, int n, int ind, int buy, int cap){
 
-        if(ind==n || cap==0) return dp[ind][buy][cap] = 0; //base case
+        if(ind==n || cap==0) return dp[ind][cap][buy] = 0; //base case
 
-        if(dp[ind][buy][cap]!=-1)
-            return dp[ind][buy][cap];
+        if(dp[ind][cap][buy]!=-1)
+            return dp[ind][cap][buy];
 
         int profit;
 
@@ -20,13 +20,13 @@ private:
                         Arr[ind] + getAns(Arr,n,ind+1,0,cap-1));
         }
 
-        return dp[ind][buy][cap] = profit;
+        return dp[ind][cap][buy] = profit;
     }    
 public:
     int maxProfit(vector<int>& prices)
     {   int n = prices.size();
         // Creating a 3d - dp of size [n][2][3]
-        dp.resize(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        dp.resize(n+1,vector<vector<int>>(3,vector<int>(2,-1)));
 
         return getAns(prices,n,0,0,2);
 
