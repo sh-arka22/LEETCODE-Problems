02953d015a020085f3fb5b0201221449fe975268
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-14 12:18:24 +0530

    Time: 2 ms (86.78%), Space: 7.9 MB (13.34%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-int tabu(int N, int M, vector<vector<int>>&grid){
-    for(int n =0;n<=N;n++){
-        for(int m=0;m<=M;m++){
-            if(n==0 and m==0){
-                dp[n][m] = 1;
-                continue;
-            }
+    int tabu(int N, int M, vector<vector<int>>&grid){
+        for(int n =0;n<=N;n++){
+            for(int m=0;m<=M;m++){
+                if(n==0 and m==0){
+                    dp[n][m] = 1;
+                    continue;
+                }
 
-            int left = 0;int right = 0;
-            if(m>0){
-                right = grid[n][m-1] ? 0 : dp[n][m-1];
+                int left = 0;int right = 0;
+                if(m>0){
+                    right = grid[n][m-1] ? 0 : dp[n][m-1];
+                }
+                if(n>0){
+                    left = grid[n-1][m] ? 0 : dp[n-1][m];
+                }
+                dp[n][m] = left + right;
             }
-            if(n>0){
-                left = grid[n-1][m] ? 0 : dp[n-1][m];
-            }
-            dp[n][m] = left + right;
         }
+        return dp[N][M];
     }
-    return dp[N][M];
-}
 
-int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-    int n = obstacleGrid.size()-1;
-    int m = obstacleGrid[0].size()-1;
-    dp.resize(n+1, vector<int>(m+1,0));
-    if(obstacleGrid[0][0] or obstacleGrid[n][m]) return 0;
-    return tabu(n,m,obstacleGrid);
-}
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size()-1;
+        int m = obstacleGrid[0].size()-1;
+        dp.resize(n+1, vector<int>(m+1,0));
+        if(obstacleGrid[0][0] or obstacleGrid[n][m]) return 0;
+        return tabu(n,m,obstacleGrid);
+    }
 };
