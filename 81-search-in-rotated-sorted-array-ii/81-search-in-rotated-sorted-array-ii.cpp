81fd3805e3ee4fa0b91e8978627af9458a8ca777
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-15 11:08:21 +0530

    Time: 12 ms (44.18%), Space: 14.1 MB (43.04%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() -1;
+        while(low<=high) {
+            int mid  = low +(high - low)/2;
+            if(target == nums[mid]){
+                return true;
+            }
+            // check if left part is sorted or not
+            if(nums[low]  < nums[mid]){
+                if(target <nums[low] || target > nums[mid]){ // target is out of the left part boundary scope
+                    low = mid +1;
+
+                } else{
+                    // target lies within this left part
+                    high = mid -1;
+                }
+            } else if(nums[low] > nums[mid]){
+                // right part is sorted
+                if(target < nums[mid] || target > nums[high]){
+                    high = mid -1;
+                } else {
+                    // target lies within the right part boundaries
+                    low = mid+1;
+                }
+            } else{
+
+                // duplicates
+                low++; // => O(n) 111111112
+            }
+
+        }
+        return false;
+    }
+};
