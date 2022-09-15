d47f16f6d3965cc736758db69b93594e8ba03e79
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-15 09:28:19 +0530

    Time: 3 ms (92.77%), Space: 8.8 MB (51.15%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -8,7 +8,7 @@ public:
         
         int left=0,right=nums.size()-1;
         
-        while(left<right){
+        while(left<=right){
             int mid = left+(right-left)/2;
             if(nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]){   
                 return mid;
