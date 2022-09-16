df198bc81382b71da4e8ce602a5ca3a0371460f4
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-16 05:44:25 +0530

    Time: 423 ms (43.15%), Space: 10.6 MB (44.33%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,25 +1,31 @@
 class Solution {
 private:
     vector<int>dp;
-    int recc(vector<int>& nums, int idx){
-        if(idx-1 < 0) return dp[idx] = 0;
-        if(dp[idx] != -1) return dp[idx];
-        int cnt = 1;
-        for(int i=idx;i>=1;i--){
-            if(nums[idx-1]>nums[i-1]){
-                int faith = recc(nums, i) ;
-                cnt = max(cnt, faith+1);
+    int recc(vector<int>& nums, int Idx){
+        for(int idx=0;idx<=Idx;idx++){
+            if(idx-1 < 0){
+                dp[idx] = 0;
+                continue;
             }
+            int cnt = 1;
+            for(int i=idx;i>=1;i--){
+                if(nums[idx-1]>nums[i-1]){
+                    int faith = dp[i];
+                    cnt = max(cnt, faith+1);
+                }
+            }
+            dp[idx] = cnt;
         }
-        return dp[idx] = cnt;
+        return 1;
     }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         int maxLen = -1;
         dp.resize(n+1, -1);
+        int call = recc(nums, n);
         for(int i=1;i<n+1;i++){
-            maxLen = max(maxLen,recc(nums, i));
+            maxLen = max(maxLen,dp[i]);
         }
         return maxLen;
     }
