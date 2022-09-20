c3432add1d6278187d2afe563472bb6700061667
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-21 05:10:39 +0530

    Time: 39 ms (93.85%), Space: 28.3 MB (10.97%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,43 +1,17 @@
 class Solution {
 public:
-  int check(std::vector<int> nums, int len)
-  {
-    int ma = 0;
-    int start = 0;
-    int end = start + len - 1;
-    for (int i = start; i <= end; ++i)
-    {
-      ma += nums[i];
+    int minSubArrayLen(int tar, vector<int>& nums) {
+        int n = nums.size();
+        int ans = (int)1e9;
+        int si = 0;
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            while(sum>=tar){
+                ans = min(ans, i-si+1);
+                sum -= nums[si++];
+            }
+        }
+        return (ans != (int)1e9) ? ans : 0;
     }
-    int sum = ma;
-    while (end < nums.size())
-    {
-      end++;
-      if (end >= nums.size())
-        break;
-      sum -= nums[start++];
-      sum += nums[end];
-      ma = max(ma, sum);
-    }
-    return ma;
-  }
-  int minSubArrayLen(int s, vector<int>& nums) {
-    int mi = 1;
-    int ma = nums.size();
-    int res = nums.size() + 1;
-    // debug(check(nums, 2));
-    while (mi <= ma)
-    {
-      int mid_size = (mi + ma) / 2;
-      int sum = check(nums, mid_size);
-      if (sum >= s)
-      {
-        ma = mid_size - 1;
-        res = min(res, mid_size);
-      }
-      else
-        mi = mid_size + 1;
-    }
-    return (res == nums.size() + 1) ? 0 : res;
-  }
 };
