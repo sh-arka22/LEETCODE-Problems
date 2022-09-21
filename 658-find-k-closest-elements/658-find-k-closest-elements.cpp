58706750313b56d5e5d2a01ec9e867650f81d508
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-21 08:12:39 +0530

    Time: 126 ms (39.70%), Space: 33.2 MB (49.40%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,19 +1,45 @@
 class Solution {
+private:
+    int lowerBound(vector<int>& arr, int x){
+        int l = 0, r = arr.size()-1;
+        int idx = -1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(arr[mid] == x) return mid;
+            else if(arr[mid]<x){
+                idx = mid;
+                l = mid+1;
+            }
+            else r = mid-1;
+        }
+        return idx;
+    }
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int,int>>pq;
-        for(auto i:arr){
-            pq.push({abs(i-x),i});
-            if(pq.size()>k) pq.pop();
+        
+        int n = arr.size();
+        int idx = lowerBound(arr, x);
+        cout<<idx;
+        int i = idx, j = idx+1;
+        
+        vector<int>ansL, ansR;
+        while(i>=0 and j<n and k--){
+            if(abs(x-arr[i]) > abs(x-arr[j])){
+                ansR.push_back(arr[j++]);
+            }
+            else ansL.push_back(arr[i--]);
         }
+        while(k-->0){
+            if(i>=0) ansL.push_back(arr[i--]);
+            if(j<n) ansR.push_back(arr[j++]);
+        }
+        reverse(ansL.begin(), ansL.end());
+        // reverse(ansR.begin(), ansR.end());
+        
         vector<int>ans;
-        while(pq.size()){
-            auto it = pq.top();
-            ans.push_back(it.second);
-            pq.pop();
-        }
-        // reverse(begin(ans), end(ans));
-        sort(begin(ans), end(ans));
+        for(auto it:ansL) ans.push_back(it);
+        for(auto it:ansR) ans.push_back(it);
+        
         return ans;
     }
 };
