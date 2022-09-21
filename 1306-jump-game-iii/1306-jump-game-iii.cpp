ad5de405d699863de8713f60155f79e94f19eecd
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-21 12:12:18 +0530

    Time: 107 ms (21.97%), Space: 30.9 MB (91.62%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
-        if (start >= 0 and start < arr.size() and arr[start] >= 0) {
-            if (arr[start] == 0) {
-                return true;
-            }
-            arr[start] = -arr[start];
-            return canReach(arr, start + arr[start]) or canReach(arr, start - arr[start]);
-        }
-        return false;
+        if(start<0 or start>=arr.size() or arr[start]<0) return false;
+        if(arr[start] ==0) return true;
+        
+        arr[start] *= -1;
+        return canReach(arr,start+arr[start]) or canReach(arr,start-arr[start]);
     }
 };
