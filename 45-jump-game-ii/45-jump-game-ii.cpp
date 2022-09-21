47309276903babf506257c5667978137fac1358c
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-21 11:38:17 +0530

    Time: 36 ms (53.72%), Space: 16.7 MB (52.28%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -5,12 +5,12 @@ public:
         int farthest = 0, currentJumpEnd = 0;
         int n = nums.size();
         
-        for(int i=0;i<n-1;i++){
-            farthest = max(farthest, nums[i]+i);
-            if(i == currentJumpEnd){
+        for(int i=0;i<n;i++){
+            if(i > currentJumpEnd){
                 jump++;
                 currentJumpEnd = farthest;
             }
+            farthest = max(farthest, nums[i]+i);
         }
         return jump;
     }
