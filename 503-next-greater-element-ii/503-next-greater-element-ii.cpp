32193212d5b00216976e642f978eaa002c1b4706
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-22 09:26:41 +0530

    Time: 74 ms (36.20%), Space: 23.9 MB (63.63%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/503-next-greater-element-ii.cpp
@@ -4,11 +4,11 @@ public:
         int n = nums.size();
         
         vector<int>res(n, -1);
-        deque<int>st;
+        stack<int>st;
         for(int i=2*n-1; i>=0; i--){
-            while(st.size() and nums[i%n]>=st.back()) st.pop_back();
-            if(st.size()) res[i%n] = st.back();
-            st.push_back(nums[i % n]);
+            while(st.size() and nums[i%n]>=st.top()) st.pop();
+            if(st.size()) res[i%n] = st.top();
+            st.push(nums[i % n]);
         }
         return res;
     }
