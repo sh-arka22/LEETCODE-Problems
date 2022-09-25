66e5dc0b83c9d330a49fc8b8c54ce16e661c29df
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-25 15:05:33 +0530

    Time: 61 ms (12.10%), Space: 14.5 MB (35.12%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,39 +1,48 @@
-class Solution {
+class Solution
+{
 public:
-     bool dfs(vector<int>&vis, vector<int>&pathVis, vector<int>adj[], int v){
+    bool dfs(vector<int> &vis, vector<int> &pathVis, vector<int> adj[], int v)
+    {
         vis[v] = 1;
         pathVis[v] = 1;
 
-        for(int child:adj[v]) {
-            if(!vis[child] and dfs(vis, pathVis, adj, child)) return true;
-            else if(pathVis[child]) return true;
+        for (int child : adj[v])
+        {
+            if (!vis[child] and dfs(vis, pathVis, adj, child))
+                return true;
+            else if (pathVis[child])
+                return true;
         }
         pathVis[v] = 0;
         return false;
     }
-    
-    bool isCyclic(int V, vector<int> adj[]) {
+
+    bool isCyclic(int V, vector<int> adj[])
+    {
         // code here
-        vector<int>vis(V,0);
-        vector<int>pathVis(V,0);
-        for(int i=0;i<V;i++){
-            if(!vis[i] and dfs(vis, pathVis, adj, i) == true)
+        vector<int> vis(V, 0);
+        vector<int> pathVis(V, 0);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i] and dfs(vis, pathVis, adj, i) == true)
                 return true;
         }
         return false;
     }
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+    {
         int v = numCourses;
         int V = v;
         int indeg[v];
-        fill(indeg, indeg+v, 0);
-        vector<int>graph[v];
-        for(vector<int>edge:prerequisites){
+        fill(indeg, indeg + v, 0);
+        vector<int> graph[v];
+        for (vector<int> edge : prerequisites)
+        {
             indeg[edge[0]]++;
             graph[edge[1]].push_back(edge[0]);
         }
 
-    bool flag =  isCyclic(V, graph);
+        bool flag = isCyclic(V, graph);
         return !flag;
     }
 };
