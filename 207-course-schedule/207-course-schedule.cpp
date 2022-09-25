57384dc7e5bb4229d9324d3b31db9e8a57f9812f
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-25 14:58:52 +0530

    Time: 48 ms (30.46%), Space: 14.2 MB (46.07%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
+     bool dfs(vector<int>&vis, vector<int>&pathVis, vector<int>adj[], int v){
+        vis[v] = 1;
+        pathVis[v] = 1;
+
+        for(int child:adj[v]) {
+            if(!vis[child] and dfs(vis, pathVis, adj, child)) return true;
+            else if(pathVis[child]) return true;
+        }
+        pathVis[v] = 0;
+        return false;
+    }
+    
+    bool isCyclic(int V, vector<int> adj[]) {
+        // code here
+        vector<int>vis(V,0);
+        vector<int>pathVis(V,0);
+        for(int i=0;i<V;i++){
+            if(!vis[i] and dfs(vis, pathVis, adj, i) == true)
+                return true;
+        }
+        return false;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int v = numCourses;
+        int V = v;
         int indeg[v];
         fill(indeg, indeg+v, 0);
         vector<int>graph[v];
@@ -9,26 +32,8 @@ public:
             indeg[edge[0]]++;
             graph[edge[1]].push_back(edge[0]);
         }
-        
-        queue<int>que;
-        
-        for(int i=0;i<v;i++){
-            if(indeg[i] == 0) que.push(i);
-        }
-        vector<int>processed;
-        
-        while(que.size()){
-            int sz = que.size();
-            while(sz--){
-                int top = que.front();
-                que.pop();
-                processed.push_back(top);
-                for(auto child: graph[top]){
-                    if(--indeg[child] == 0)
-                        que.push(child);
-                }
-            }
-        }
-        return processed.size() == v ? 1 : 0;
+
+    bool flag =  isCyclic(V, graph);
+        return !flag;
     }
 };
