5ab5307f396d442207fa87313144783be4909d9f
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-27 00:34:07 +0530

    Time: 404 ms (56.09%), Space: 81.4 MB (92.25%) - LeetHub

--- 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
+++ 1712-ways-to-split-array-into-three-subarrays/1712-ways-to-split-array-into-three-subarrays.cpp
@@ -4,13 +4,12 @@ public:
         long mod = 1e9+7, ans = 0;
         for (int i = 1; i < A.size(); ++i) A[i] += A[i - 1];
         long N = A.size(), i = 0, j = 0, k = 0;
-        for (; i < N-2; ++i) {
+        for (; i <= N-3; ++i) {
             long left = A[i];
             j = max(i + 1, j); // `j` is at least one greater than `i`.
-            while (j < N && A[j] - left < left) ++j; // find the smallest `j` that satisfies `mid >= left`
-            // if (j >= N) break; // No room for `k`. Break
+            while (j <= N-1 && A[j] - left < left) ++j; // find the smallest `j` that satisfies `mid >= left`
             k = max(k, j);
-            while (k < N - 1 && A.back() - A[k] >= A[k] - A[i]) ++k;
+            while (k <= N - 2 && A.back() - A[k] >= A[k] - A[i]) ++k;
             ans = (ans + k - j) % mod;
         }
         return ans;
