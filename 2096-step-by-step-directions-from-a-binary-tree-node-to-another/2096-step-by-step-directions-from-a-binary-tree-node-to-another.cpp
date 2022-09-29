e440412dcdcab146ff404054df61840c7685aea7
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-29 16:43:43 +0530

    Time: 393 ms (47.10%), Space: 112.2 MB (92.71%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -11,72 +11,23 @@
  */
 class Solution {
 public:
-    int *parrent, *vis;
-    char *dirArray;
-    int countNodes(TreeNode* root){
-        if(!root) return 0;
-        int leftCount = countNodes(root->left);
-        int rightCount = countNodes(root->right);
-        return leftCount+rightCount+1;
-    }
-    void createGraph(TreeNode* root, vector<pair<int,char>>graph[]){
-        if(!root) return;
-        if(root->left){
-            graph[root->val].push_back({root->left->val,'L'});
-            graph[root->left->val].push_back({root->val, 'U'});
-            
-        }
-        if(root->right){
-            graph[root->val].push_back({root->right->val,'R'});
-            graph[root->right->val].push_back({root->val, 'U'});
-            
-        }
-        createGraph(root->right, graph);
-        createGraph(root->left, graph);
+    bool find(TreeNode* n, int val, string &path) {
+        if (n->val == val)
+            return true;
+        if (n->left && find(n->left, val, path))
+            path.push_back('L');
+        else if (n->right && find(n->right, val, path))
+            path.push_back('R');
+        return !path.empty();
     }
     string getDirections(TreeNode* root, int startValue, int destValue) {
-        int nodes = 0;
-        nodes = countNodes(root);
-        vector<pair<int,char>>graph[nodes+1];
-        createGraph(root, graph);
-        parrent = new int[nodes+1];
-        vis = new int[nodes+1];
-        fill(vis,vis+nodes+1,0);
-        dirArray = new char[nodes+1];
-        parrent[startValue] = -1;
-        queue<int>que;
-        que.push(startValue);
-        while(que.size()){
-            int sz = que.size();
-            while(sz--){
-                int parr = que.front(); que.pop();
-                if(parr == destValue) break;
-                for(auto[child,dir]:graph[parr]){
-                    if(vis[child]) continue;
-                    vis[child] = 1;
-                    dirArray[child] = dir;
-                    parrent[child] = parr;
-                    que.push(child);
-                }
-            }
+        string s_p, d_p;
+        find(root, startValue, s_p);
+        find(root, destValue, d_p);
+        while (!s_p.empty() && !d_p.empty() && s_p.back() == d_p.back()) {
+            s_p.pop_back();
+            d_p.pop_back();
         }
-        
-        string res = "";
-        // for(auto it:parrent) cout<<it<<" ";
-        for(int i=0;i<=nodes;i++){
-            cout<<parrent[i]<<" ";
-        }
-        cout<<dirArray[destValue];
-        while(parrent[destValue] != -1){
-            res+=dirArray[destValue];
-            destValue = parrent[destValue];
-            if(destValue == startValue) break;
-        }
-        
-        reverse(res.begin(), res.end());
-        delete[]parrent;
-        delete[]vis;
-        delete[]dirArray;
-        return res;
+        return string(s_p.size(), 'U') + string(rbegin(d_p), rend(d_p));
     }
 };
