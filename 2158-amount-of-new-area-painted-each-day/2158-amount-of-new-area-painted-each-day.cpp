8c738be7d6a4cff5ecfa79c336a298424b83a2cb
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-29 19:10:06 +0530

    Time: 865 ms (48.15%), Space: 131.9 MB (81.82%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
+    vector<int>par;
+    int n;
+    
+    void make_set(int v){
+        par[v] = v;
+    }
+    
+    int find_set(int v){
+        if(par[v]==v)
+            return v;
+        return par[v] = find_set(par[v]);
+    }
+    
+    void union_set(int u, int v){
+        u = find_set(u);
+        v = find_set(v);
+        
+        par[u] =v;
+    }
+    
     vector<int> amountPainted(vector<vector<int>>& paint) {
-        map<int,int>mp;
-        int total = 0;
-        int cnt = 0;
-        vector<int>ans;
-        for(auto it:paint){
-            int start = it[0], end = it[1];
-            while(start < end){
-                if(mp.count(start)==0){
-                    mp[start] = end;
-                    start++;
-                    cnt++;
-                }
-                else{
-                    // start = mp[start];
-                    
-                    int prev_end = mp[start];
-                    mp[start] = max(mp[start],end);
-                    start = prev_end;   
-                }
-            }
-            ans.push_back(cnt);
-            total += cnt;
-            cnt = 0; 
+        int maxlen = 0;
+        int minlen = INT_MAX;
+        for(auto& v:paint){
+            minlen = min(minlen, v[0]);
+            maxlen = max(maxlen, v[1]);
         }
+        par.resize(maxlen+2);
+        
+        for(int i=minlen; i<=maxlen+1; i++){
+            make_set(i);
+        }
+        n = paint.size();
+        vector<int>ans(n,0);
+        
+        for(int i=0; i< paint.size(); i++){
+            int l = paint[i][0]; int r = paint[i][1];
+            
+            for(l=find_set(l); l<r; l=find_set(l)){
+                ans[i]++;
+                union_set(l, l+1);
+            }
+        }
+        
         return ans;
     }
 };
