a10fb471f96720881d88652fa6c29382157e31fe
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-29 18:43:22 +0530

    Time: 1246 ms (17.42%), Space: 163.7 MB (30.86%) - LeetHub

---
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> amountPainted(vector<vector<int>>& paint) {
+        unordered_map<int,int>mp;
+        int total = 0;
+        int cnt = 0;
+        vector<int>ans;
+        for(auto it:paint){
+            int start = it[0], end = it[1];
+            while(start < end){
+                if(mp.count(start)==0){
+                    mp[start] = end;
+                    start++;
+                    cnt++;
+                }
+                else{
+                    int prev_end = mp[start];
+                    mp[start] = max(mp[start],end);
+                    start = prev_end;   
+                }
+            }
+            ans.push_back(cnt);
+            total += cnt;
+            cnt = 0; 
+        }
+        return ans;
+    }
+};
