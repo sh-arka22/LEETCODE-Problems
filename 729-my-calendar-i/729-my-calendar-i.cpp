95b8dcf804b1fa0b7f3bfc7e20afec3f824db2bd
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-29 22:38:32 +0530

    Time: 662 ms (5.00%), Space: 44.1 MB (5.73%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,13 +1,28 @@
 class MyCalendar {
-private:
-    map<int,int>events;
 public:
-    MyCalendar() {   
+    map<int,int>mp;
+    MyCalendar() {
+        
     }
+    
     bool book(int start, int end) {
-        auto next = events.upper_bound(start);
-        if(next != events.end() &&   (*next).second < end)return false;
-        events.insert({end,start});
+        int sum = 0;
+        mp[start]++;
+        mp[end]--;
+        for(auto it:mp){
+            sum+=(it.second);
+            if(sum>1){
+                mp[start]--;
+                mp[end]++;
+                return false;
+            }
+        }
         return true;
     }
 };
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
