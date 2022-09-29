e9f70f55586dc94310c6024443e1ea1bea7e19b6
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-29 12:57:54 +0530

    Time: 123 ms (63.70%), Space: 31.9 MB (52.69%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int n = grid.size();
-        int m = grid[0].size();
-        queue<pair<int,pair<int,int>>> PendingNodes;
-        vector<vector<vector<bool>>> visited(n,vector<vector<bool>>(m,vector<bool>(k+1,false)));
-        PendingNodes.push({k,{0,0}});
-        visited[0][0][k] = true;
-        const int dir[4][2] = {{1,0},{-1,0},{0,-1},{0,1}};
-        while(!PendingNodes.empty()){
-            auto Front = PendingNodes.front();
-            PendingNodes.pop();
-            int currentK = Front.first;
-            int x = Front.second.first/m;
-            int y = Front.second.first%m;
-            int moves = Front.second.second;
-            
-            if(x == n-1 && y == m-1){
-                return moves;
-            }
-            
-            for(int i = 0 ; i < 4 ; i++){
-                int newX = x+dir[i][0];
-                int newY = y+dir[i][1];
-                int NewK = currentK;
-                if(newX >= 0 && newY >= 0 && newX < n && newY < m){
-                    if(grid[newX][newY])
-                        NewK--;
-                    
-                    if(NewK < 0 || visited[newX][newY][NewK])
-                        continue;
-                    PendingNodes.push({NewK,{newX*m+newY,moves+1}});
-                    visited[newX][newY][NewK] = true;
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<vector<int>>>vis(n,vector<vector<int>>(m,vector<int>(k+1,0)));
+        deque<array<int,3>>que;
+        que.push_back({0,0,k});
+        int steps = 0;
+        int dir[4][2] = {{-1,0}, {0,-1},{1,0},{0,1}};
+        while(que.size()){
+            int sz = que.size();
+            while(sz--){
+                auto [r,c,obs] = que.front();
+                // path = path + "(" + to_string(r) + "," + to_string(c)  + "," + to_string(obs) + ")" + "->";
+                que.pop_front();
+                if(r == n-1 and c == m-1) return steps;
+                for(auto &[dr, dc]:dir){
+                    int a = r+dr, b = c+dc;
+                    if(a>=0 and b>=0 and a<n and b<m){
+                        if(grid[a][b]==0 and !vis[a][b][obs]){
+                            que.push_back({a,b,obs});
+                            vis[a][b][obs] = 1;
+                        }
+                        else if(grid[a][b]==1 and obs>0 and !vis[a][b][obs-1]){
+                            que.push_back({a,b,obs-1});
+                            vis[a][b][obs-1] = 1;
+                        }
+                    }
                 }
             }
+            steps++;
         }
         return -1;
     }
