efadb5dd066f09ad04aba863e69b8fb106b366b1
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-30 23:48:12 +0530

    Time: 7 ms (84.72%), Space: 13.8 MB (92.97%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,37 +1,41 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int target) {
+    int search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size() -1;
         while(low<=high) {
             int mid  = low +(high - low)/2;
             if(target == nums[mid]){
-                return true;
+                return 1;
             }
             // check if left part is sorted or not
             if(nums[low]  < nums[mid]){
-                if(target <nums[low] || target > nums[mid]){ // target is out of the left part boundary scope
+                if(target < nums[low] or target > nums[mid]){ // target is out of the left part boundary scope
                     low = mid +1;
 
-                } else{
+                } 
+                else{
                     // target lies within this left part
                     high = mid -1;
                 }
-            } else if(nums[low] > nums[mid]){
+            } 
+            else if(nums[low] > nums[mid]){
                 // right part is sorted
                 if(target < nums[mid] || target > nums[high]){
                     high = mid -1;
-                } else {
+                } 
+                else {
                     // target lies within the right part boundaries
                     low = mid+1;
                 }
-            } else{
+            } 
+            else if(nums[low] == nums[mid]){
 
                 // duplicates
                 low++; // => O(n) 111111112
             }
 
         }
-        return false;
+        return 0;
     }
 };
