4ed573eacf959cec5acb0c1ccb4c6f1f01dd6d70
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-09-30 23:28:29 +0530

    Time: 4 ms (75.78%), Space: 11 MB (74.91%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int tar) {
-        int n = nums.size();
-        int l = 0, r = n-1;
-        while(l<=r){
-            int m =(l+r)/2;
-            if(nums[m] == tar) return m;
-            if(nums[l]<=nums[m]){
-                if(tar<nums[m] and tar>=nums[l])
-                    r = m-1;
-                else l = m+1;
+    int search(vector<int>& nums, int target) {
+        int low = 0;
+        int high = nums.size() -1;
+        while(low<=high) {
+            int mid  = low +(high - low)/2;
+            if(target == nums[mid]){
+                return mid;
             }
-            else if(nums[r]>=nums[m]){
-                if(tar>nums[m] and tar<=nums[r])
-                    l = m+1;
-                else r = m-1;
+            // check if left part is sorted or not
+            if(nums[low]  < nums[mid]){
+                if(target <nums[low] || target > nums[mid]){ // target is out of the left part boundary scope
+                    low = mid +1;
+
+                } else{
+                    // target lies within this left part
+                    high = mid -1;
+                }
+            } else if(nums[low] > nums[mid]){
+                // right part is sorted
+                if(target < nums[mid] || target > nums[high]){
+                    high = mid -1;
+                } else {
+                    // target lies within the right part boundaries
+                    low = mid+1;
+                }
+            } else{
+
+                // duplicates
+                low++; // => O(n) 111111112
             }
+
         }
         return -1;
     }
