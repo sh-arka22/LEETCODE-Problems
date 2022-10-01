cef5eea2eb4df0008d0297e05f0b7dd60fd75b28
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-01 12:00:19 +0530

    Time: 513 ms (97.66%), Space: 163.6 MB (41.02%) - LeetHub

--- 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.cpp
+++ 1610-maximum-number-of-visible-points/1610-maximum-number-of-visible-points.cpp
@@ -1,41 +1,38 @@
 class Solution {
-public:
-    double PI = 3.141592;
-    int visiblePoints(vector<vector<int>>& points, int angle, vector<int>& location) {
-	    // 1 Move your location to (0, 0) and move all points.
-        for (auto & p : points) {
-            p[0] -= location[0];
-            p[1] -= location[1];
-        }
-        
-        vector<double> angles;
-        int atLocation = 0;
-        for (auto & p : points) {
-            // 2 Keep count on all points already at location. 
-            if (p[0] == 0 && p[1] == 0)
-                atLocation++;
-            else // 3 Use atan2 to convert all points into angles array.
-                angles.push_back(atan2(p[1], p[0]));
-        }
-        
-        // 4 Sort angles array ascendingly.
-        sort(angles.begin(), angles.end());
-        
-        // 5 Copy each angle by adding 360 degree(2*PI) and append to angles array.
-        int size = angles.size();
-        for (int i = 0; i < size; i++) {
-            angles.push_back(2 * PI + angles[i]);
-        }
-        
-        double angleWindow = 1.0 * angle / 180 * PI;
-        int res = 0;
-        // 6 For each angle, see how many angles are in the visible window.
-        for (int i = 0; i < angles.size() / 2; i++) {
-            auto it = upper_bound(angles.begin() + i, angles.end(), angles[i] + angleWindow);  
-            it--; 
-            res = max(res, int(it - angles.begin()) -  i + 1);
-        }
-        
-        return res + atLocation;
+ public:
+  int visiblePoints(vector<vector<int>>& points, int angle,vector<int>& location) {
+    const int posX = location[0];
+    const int posY = location[1];
+    int maxVisible = 0;
+    int same = 0;
+    vector<double> pointAngles;
+
+    for (const vector<int>& p : points) {
+      const int x = p[0];
+      const int y = p[1];
+      if (x == posX && y == posY)
+        ++same;
+      else
+        pointAngles.push_back(getAngle(y - posY, x - posX));
     }
+
+    sort(begin(pointAngles), end(pointAngles));
+
+    const int n = pointAngles.size();
+    for (int i = 0; i < n; ++i)
+      pointAngles.push_back(pointAngles[i] + 360);
+
+    for (int l = 0, r = 0; r < pointAngles.size(); ++r) {
+      while (pointAngles[r] - pointAngles[l] > angle)
+        ++l;
+      maxVisible = max(maxVisible, r - l + 1);
+    }
+
+    return maxVisible + same;
+  }
+
+ private:
+  double getAngle(int dy, int dx) {
+    return atan2(dy, dx) * 180 / M_PI;
+  }
 };
