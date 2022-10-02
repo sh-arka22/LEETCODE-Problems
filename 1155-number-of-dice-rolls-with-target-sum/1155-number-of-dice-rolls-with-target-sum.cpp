47451d4318391bae7ac724d6cdbb4c0cf52b10c7
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-02 13:33:15 +0530

    Time: 716 ms (5.19%), Space: 269.4 MB (5.01%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,26 @@
+class Solution{
+private:
+    vector<vector<int>>dp;
+    int knapSack(vector<int>coins, int tar, int n){
+        if(n==0 or tar==0){
+            if(n==0 and tar==0) return 1;
+            return 0;
+        }
+        if(dp[tar][n] != -1) return dp[tar][n];
+        int cnt = 0;
+        for(auto coin:coins){
+            if(tar-coin < 0) continue;
+            cnt = (cnt + (knapSack(coins, tar-coin, n-1)%1000000007))%1000000007;
+        }
+        return dp[tar][n] = cnt;
+    }
+
+public:
+    int numRollsToTarget(int n, int k, int tar) {
+        vector<int>coins(k);
+        vector<int>vis(k+1,0);
+        dp.resize(1002, vector<int>(32,-1));
+        for(int i=1;i<=k;i++) coins[i-1] = i;
+        return knapSack(coins, tar, n);
+    }
+};
