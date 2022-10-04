11faf04322bacd20371cf9590f039abbb6b2e4a6
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-04 16:11:19 +0530

    Time: 215 ms (5.12%), Space: 17.4 MB (8.12%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -8,20 +8,45 @@
  * };
  */
 class Solution {
-private:
-    TreeNode* dfs(TreeNode* root, TreeNode* p, TreeNode* q){
-        if(!root or root == p or root == q) return root;
-        TreeNode* leftLca = dfs(root->left, p, q);
-        TreeNode* rightLca = dfs(root->right, p, q);
-        
-        if(leftLca and rightLca) return root;
-        if(leftLca) return leftLca;
-        if(rightLca) return rightLca;
-        
-        return nullptr;
-    }
 public:
+    
+    bool flag(TreeNode *root, int tar, vector<TreeNode *> &ans)
+    {
+        if (!root)
+            return 0;
+        if (root->val == tar)
+        {
+            ans.push_back(root);
+            return true;
+        }
+        if (flag(root->left, tar, ans))
+        {
+            ans.push_back(root);
+            return 1;
+        }
+        if (flag(root->right, tar, ans))
+        {
+            ans.push_back(root);
+            return 1;
+        }
+        return flag(root->left, tar, ans) or flag(root->right, tar, ans);
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return dfs(root, p, q);
+        vector<TreeNode *> list1, list2;
+        flag(root,p->val,list1);
+        flag(root,q->val,list2);
+        
+        int i = list1.size()-1;
+        int j = list2.size()-1;
+        TreeNode* LCA = nullptr;
+        while(i>=0 and j>=0){
+            if(list1[i] != list2[j]){
+                break;
+            }
+            LCA = list1[i];
+            i--;
+            j--;
+        }
+        return LCA;
     }
 };
