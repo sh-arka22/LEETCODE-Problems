e4daf79619a2632e8b09676e64983079371f4a97
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-07 13:16:27 +0530

    Time: 25 ms (19.38%), Space: 11.3 MB (9.99%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        vector<int>graph[n];
+        for(int r=0;r<n;r++){
+            for(auto k:rooms[r]){
+                graph[r].push_back(k);
+            }
+        }
+        set<int>seen;
+        deque<int>que;
+        que.push_back(0);
+        seen.insert(0);
+        
+        while(que.size()){
+            int node = que.front();
+            que.pop_front();
+            for(auto child: graph[node]){
+                if(seen.count(child))continue;
+                seen.insert(child);
+                que.push_back(child);
+            }
+        }
+        return seen.size()==n;
+    }
+};
