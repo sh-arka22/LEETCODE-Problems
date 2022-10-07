e716edb5f84e0151ba1b788c5150b0626d12cb10
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-07 11:32:50 +0530

    Time: 61 ms (19.79%), Space: 14.4 MB (32.93%) - LeetHub

--- 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 323-number-of-connected-components-in-an-undirected-graph/323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,25 +1,28 @@
 class Solution {
-public:
-    const static int N = 1e4;
-    int parr[N];
-    int findParr(int v){
-        return parr[v] == v ? v : (parr[v] = findParr(parr[v]));
-    }
-    int countComponents(int n, vector<vector<int>>& edges) {
-        for(int i=0;i<n;i++) parr[i] = i;
-        
-        int componenets = n;
-        
-        for(vector<int>edge:edges){
-            int v = edge[0], u = edge[1];
-            int p1 = findParr(v);
-            int p2 = findParr(u);
-            
-            if(p1 != p2){
-                componenets--;
-                parr[p2] = p1;
-            }
+private:
+    void dfs(vector<int>graph[], int vis[], int v){
+        vis[v] = 1;
+        for(int u:graph[v]){
+            if(vis[u]) continue;
+            dfs(graph, vis, u);
         }
-        return componenets;
+    }
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<int>graph[n];
+        for(auto edge : edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        int cnt = 0; 
+        int vis[n];
+        fill(vis, vis+n, 0);
+        for(auto i=0;i<n;i++){
+            if(vis[i]) continue;
+            vis[i] = 1;
+            dfs(graph, vis, i);
+            cnt++;
+        }
+        return cnt;
     }
 };
