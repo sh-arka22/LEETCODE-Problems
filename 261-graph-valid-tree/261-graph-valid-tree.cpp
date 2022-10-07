a4b50c645b99b062ded001bb2eac0d74437b956e
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-07 12:08:44 +0530

    Time: 50 ms (22.09%), Space: 12.7 MB (50.07%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.cpp
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -1,40 +1,26 @@
 class Solution {
-    vector<int>size;
-    vector<int>parr;
-    int maxi = 0;
-    int findParr(int v){
-        return v == parr[v] ? v : (parr[v] = findParr(parr[v]));
-    }
-    void union_(int p1, int p2){
-        if(size[p1]>size[p2]){
-            // for(auto it: parr) cout<<it<<" ";
-            parr[p2] = p1;
-            size[p1] += size[p2];
-            maxi = max(maxi,size[p1]);
-        }
-        else{
-            parr[p1] = p2;
-            size[p2] += size[p1];
-            maxi = max(maxi,size[p2]);
-        }
-    }
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        if(edges.size() == 0 and n == 1) return 1;
-        for(int i=0;i<n;i++){
-            size.push_back(1);
-            parr.push_back(i);
+        if(edges.size() != n-1) return 0;
+        vector<int>graph[n];
+        for(auto edge : edges){
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
+        deque<int>que;
+        set<int>vis;
+        que.push_back(0);
+        vis.insert(0);
         
-        for(auto &edge:edges){
-            int v = edge[0], u = edge[1];
-            int p1 = findParr(v);
-            int p2 = findParr(u);
-            if(p1 != p2)
-                union_(p1,p2);
-            else return false;
+        while(que.size()){
+            int node = que.front();
+            que.pop_front();
+            for(int child: graph[node]){
+                if(vis.count(child)) continue;
+                vis.insert(child);
+                que.push_back(child);
+            }
         }
-        
-        return maxi == n ? 1 : 0;
+        return vis.size()==n;
     }
 };
