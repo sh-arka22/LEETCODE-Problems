9218e2a24e045ae9ea64a8172995b535d7f91c55
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-16 17:46:25 +0530

    Time: 691 ms (29.98%), Space: 10.4 MB (71.12%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,24 +1,16 @@
 class Solution {
-private:
-   int search(vector<int>& nums){
-       int n = nums.size();
-       vector<int>tmp;
-       int len = 1;
-       tmp.push_back(nums[0]);
-       for(int i=1;i<n;i++){
-           if(nums[i]>tmp.back()) {
-               tmp.push_back(nums[i]);
-               len++;
-           }
-           else{
-               int idx = lower_bound(tmp.begin(), tmp.end(), nums[i]) - tmp.begin();
-               tmp[idx] = nums[i];
-           }
-       }
-       return len;
-   } 
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        return search(nums);
+    int lengthOfLIS(vector<int>& arr) {
+        int n = arr.size();
+    int maxi = 1;
+    vector<int>dp(n,1);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(arr[i]>arr[j] and dp[j]+1 > dp[i])
+                dp[i] = dp[j]+1;
+        }
+        maxi = max(maxi, dp[i]);
+    }
+    return maxi;
     }
 };
