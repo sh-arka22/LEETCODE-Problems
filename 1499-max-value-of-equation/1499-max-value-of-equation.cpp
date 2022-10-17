ef8694a819caf116a1719689c557a68a3ac07e3c
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-17 20:07:28 +0530

    Time: 823 ms (10.33%), Space: 109 MB (10.73%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -7,7 +7,7 @@ public:
         int j = 0;
         for(int i=0;i<n;i++){
             for(;j<n and points[j][0] - points[i][0] <= k;j++) st.insert(points[j][0]+points[j][1]);
-            st.erase(st.find(points[i][0] + points[i][1]));
+            st.erase(st.find(points[i][0] + points[i][1])); //this coordinate cant be a part of the ans
             if(st.size()) ans = max(ans, points[i][1]-points[i][0]+*st.rbegin());
         }
         return ans;
