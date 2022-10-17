0423101f0d45bb1835b3a3eac2e126c91aa37637
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-17 19:42:26 +0530

    Time: 441 ms (73.37%), Space: 109.2 MB (8.97%) - LeetHub

---
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
+        int n = points.size();
+        int ans = -(int)1e9;
+        multiset<int>st;
+        int j = 0;
+        for(int i=0;i<n;i++){
+            for(;j<n and points[j][0] - points[i][0] <= k;j++) st.insert(points[j][0]+points[j][1]);
+            st.erase(st.find(points[i][0] + points[i][1]));
+            if(st.size()) ans = max(ans, points[i][1]-points[i][0]+*st.rbegin());
+        }
+        return ans;
+    }
+};
