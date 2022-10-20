13da58549bfe37cb09b617028c2fdfae2d5f29a7
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-20 17:16:16 +0530

    Time: 22 ms (75.44%), Space: 7.8 MB (48.76%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int>tar, cnt;
-        for(char c:t){
-            tar[c]++;
-        }
-        int n = s.size();
-        int match = 0;
-        int len = INT_MAX;
-        int l = 0, r = 0;
+        vector<int>tar(128,0), cnt(128,0);
+        for(auto c:t) tar[c]++;
+        int len = (int)1e9;
+        int l = 0, r = 0, n = size(s), m = size(t), match = 0;
         int begin = 0;
         while(r<n){
-            cnt[s[r]]++;
-            if(cnt[s[r]] <= tar[s[r]])
+            cnt[s[r]] ++;
+            if(cnt[s[r]]<=tar[s[r]])
                 match++;
-            while(match == t.size()){
+            while(match == m){
                 if(r-l+1 < len){
                     begin = l;
                     len = r-l+1;
                 }
                 cnt[s[l]]--;
-                if(cnt[s[l]] < tar[s[l]]){
+                if(cnt[s[l]] < tar[s[l]])
                     match--;
-                }
                 l++;
             }
             r++;
         }
-        return len == INT_MAX ? "" : s.substr(begin,len);
+        return len == (int)1e9 ? "" : s.substr(begin,len);
     }
 };
