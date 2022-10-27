b26f00d3f751100bb4a5bb975126de749e57c81a
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-27 12:14:27 +0530

    Time: 333 ms (39.19%), Space: 71.7 MB (24.32%) - LeetHub

--- 0710-random-pick-with-blacklist/0710-random-pick-with-blacklist.cpp
+++ 0710-random-pick-with-blacklist/0710-random-pick-with-blacklist.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-
-    unordered_map<int, int> m;
-    int wlen;
-
-    Solution(int n, vector<int> b) {
-        wlen = n - b.size();
-        unordered_set<int> w;
-        for (int i = wlen; i < n; i++) w.insert(i);
-        for (int x : b) w.erase(x);
-        auto wi = w.begin();
-        for (int x : b)
-            if (x < wlen)
-                m[x] = *wi++;
+    unordered_map<int,int>mp;
+    int top;
+    Solution(int n, vector<int>& blacklist) {
+        top = n-size(blacklist);
+        unordered_set<int>st;
+        for(auto x:blacklist) st.insert(x);
+        
+        n--;
+        for(int i=0;i<size(blacklist);i++){
+            if(blacklist[i]<top){
+                while(st.count(n)) n--;
+                mp[blacklist[i]]=n;
+                n--;
+            }
+        }
     }
-
+    
     int pick() {
-        int k = rand() % wlen;
-        return m.count(k) ? m[k] : k;
+        int ans = rand()%top;
+        if(mp.count(ans))
+            return mp[ans];
+        return ans;
     }
 };
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(n, blacklist);
+ * int param_1 = obj->pick();
+ */
