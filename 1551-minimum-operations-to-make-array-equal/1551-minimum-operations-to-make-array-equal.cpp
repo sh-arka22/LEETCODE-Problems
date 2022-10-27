3287c21198ca052eaa57e251260ddb3c6a26e3d5
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-28 02:23:28 +0530

    Time: 21 ms (9.29%), Space: 11.4 MB (13.08%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int minOperations(int n) {
-        return n % 2 == 0 ? n * n / 4 : (n * n - 1) / 4;
+         vector<int> nums(n);
+        for(int i=0;i<n;i++)
+            nums[i]=(2*i)+1;
+        int mid = nums[(nums.size()-1)/2];
+        int count = 0;
+        for(int i = 0;i<nums.size();i++){
+            count = count + abs(nums[i]-mid);
+        }
+        return count/2;
     }
 };
