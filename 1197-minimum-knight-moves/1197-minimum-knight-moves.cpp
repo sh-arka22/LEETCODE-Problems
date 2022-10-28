1434377f1101550a7f4e8aac3d28e3269c540703
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-29 04:51:26 +0530

    Time: 1444 ms (26.74%), Space: 36.8 MB (62.87%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
+    const static int N = 301;
     int minKnightMoves(int x, int y) {
-        // Adjust to 1/8.
-        if (x < 0) { x = -x; }
-        if (y < 0) { y = -y; }
-        if (x < y) { swap(x, y); }
-        
-        // Corner case.
-        if (x == 1 and y == 0) { return 3; }
-        if (x == 2 and y == 2) { return 4; }
-
-        // Core formula.
-        if (y <= x / 2) {
-            return (x + 1) / 2 + (x / 2 - y) % 2; // Lower part.
-        } else {
-            return (x + y) / 3 + (x + y) % 3; // Higher part.
+        int dir[8][2] = {{2,1}, {2,-1}, {-2,1}, {-2,-1}, {-1,2}, {1,2}, {1,-2}, {-1,-2}};
+        deque<array<int,2>>que;
+        unordered_set<string>vis;
+        que.push_back({0,0});
+        x=abs(x);
+        y=abs(y);
+        int rad = 0;
+        while(que.size()){
+            int sz = que.size();
+            while(sz--){
+                auto [xVal,yVal]=que.front();
+                que.pop_front();
+                if(xVal==x and yVal==y) return rad;
+                
+                if(xVal<-2 or yVal<-2) continue;
+                if(xVal>x+2 or yVal>y+2) continue;
+                
+                for(auto &[dr, dc]:dir){
+                    int nX = xVal+dr;
+                    int nY = yVal+dc;
+                    if(vis.count(to_string(nX)+ to_string(nY))) continue;
+                    que.push_back({nX,nY});
+                    vis.insert(to_string(nX)+ to_string(nY));
+                }
+            }
+            rad++;
         }
+        return rad;
     }
 };
