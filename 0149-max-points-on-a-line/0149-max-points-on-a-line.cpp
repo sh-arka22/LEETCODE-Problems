f8e1d0c9c0a83f6479c44f5d1ae4bb12ce3c4203
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-10-31 04:59:06 +0530

    Time: 132 ms (31.02%), Space: 26.7 MB (9.05%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int ans = 0;
+        int n = points.size();
+        if(n < 3)
+            return n;
+        for(int i = 0;i<n;i++){
+            unordered_map<string, int> mp;
+            for(int  j = 0;j<n;j++){
+                if(i != j){
+                    int dy = (points[j][1] - points[i][1]);
+                    int dx = (points[j][0] - points[i][0]);
+                    int g = __gcd(dy,dx);
+                    mp[to_string(dy/g)+"#"+to_string(dx/g)]++;
+                }
+            } 
+            for(auto k : mp){
+                ans = max(ans,k.second);
+            }
+        }
+        return ans+1;// extra 1 because it didn't indclue itself.
+    }
+};
