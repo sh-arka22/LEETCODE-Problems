d5b571eae09273f2775e9bfa0ca90826f561caca
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-11-01 05:25:06 +0530

    Time: 257 ms (96.74%), Space: 105.3 MB (42.13%) - LeetHub

--- 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.cpp
+++ 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.cpp
@@ -1,30 +1,48 @@
 class Solution {
 public:
-    int totalStrength(vector<int>& s) {
-    long long res = 0, sz = s.size(), mod = 1000000007;
-    vector<int> ps_l(sz + 1), pm_l(sz + 1); // prefix sum and prefix mul from the left.
-    vector<int> ps_r(sz + 1), pm_r(sz + 1); // ... from the right.
-    vector<int> st; // mono-increasing stack.
-    for (long long i = 0; i < sz; ++i) {
-        ps_l[i + 1] = (ps_l[i] + s[i]) % mod;
-        pm_l[i + 1] = (pm_l[i] + (i + 1) * s[i]) % mod;
-    }
-    for (int i = sz - 1; i >= 0; --i) {
-        ps_r[i] = (ps_r[i + 1] + s[i]) % mod;
-        pm_r[i] = (pm_r[i + 1] + (sz - i) * s[i]) % mod;
-    }        
-    for (int right = 0; right <= sz; ++right) {
-        while(!st.empty() && (right == sz || s[st.back()] >= s[right])) {
-            int pivot = st.back();
-            st.pop_back();
-            long long left = st.empty() ? 0 : st.back() + 1;
-            long long left_sum = (mod + pm_l[pivot + 1] - pm_l[left] - left * (ps_l[pivot + 1] - ps_l[left]) % mod) % mod;
-            long long right_sum = (mod + pm_r[pivot + 1] - pm_r[right] - (sz - right) * (ps_r[pivot + 1] - ps_r[right])) % mod;
-            long long all_sum = (right_sum * (pivot - left + 1) + left_sum * (right - pivot)) % mod;
-            res = (res + all_sum * s[pivot]) % mod;
+    int totalStrength(vector<int>& st) {
+        long long MOD = 1'000'000'007;
+        const int N = st.size();
+        // sum of first k elements
+        vector<long long> prefix(N + 1, 0L);
+        for (int i = 0; i < N; ++i) {
+            prefix[i + 1] = (prefix[i] + st[i]) % MOD;
         }
-        st.push_back(right);
+        // sum of first k prefix
+        vector<long long> prefix_sum(N + 2, 0L);
+        for (int i = 0; i <= N; ++i) {
+            prefix_sum[i + 1] = (prefix_sum[i] + prefix[i]) % MOD;
+        }
+
+        // first index on the left < current st
+        vector<int> left(N, -1);
+        // mono increase
+        vector<int> stack;
+        for (int i = 0; i < N; ++i) {
+            while (!stack.empty() && st[stack.back()] >= st[i]) {
+                stack.pop_back();
+            }
+            left[i] = stack.empty() ? -1 : stack.back();
+            stack.push_back(i);
+        }
+
+        // first index on the right <= current st
+        vector<int> right(N, N);
+        stack.clear();
+        for (int i = N - 1; i >= 0; --i) {
+            while (!stack.empty() && st[stack.back()] > st[i]) {
+                stack.pop_back();
+            }
+            right[i] = stack.empty() ? N : stack.back();
+            stack.push_back(i);
+        }
+
+        long long res = 0;
+        for (int i = 0; i < N; ++i) {
+            res += ((prefix_sum[right[i] + 1] - prefix_sum[i + 1]) * (i - left[i]) % MOD + MOD * 2 - 
+                   (prefix_sum[i + 1] - prefix_sum[left[i] + 1]) * (right[i] - i) % MOD) % MOD * st[i] % MOD;
+            res %= MOD;
+        }
+        return (int) res;
     }
-    return res;
-}
 };
