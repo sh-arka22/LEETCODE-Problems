62649a854362c134e94b71929545e62bb1a3449d
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-11-02 04:37:35 +0530

    Time: 1439 ms (13.66%), Space: 10.2 MB (87.95%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -4,14 +4,14 @@ private:
         int dp[1001][1001] = { 1 };
         for(int n=0;n<=N;n++){
             for(int k=0;k<=K;k++){
-                if(n==0){
-                    dp[n][k] = 0;
+                if(n==0 or k==0){
+                    dp[n][k] = k==0;
                     continue;
                 }
-                if(k==0){
-                    dp[n][k]=1;
-                    continue;
-                }
+                // if(k==0){
+                //     dp[n][k]=1;
+                //     continue;
+                // }
                 int inv = 0;
                 for(int i=0;i<=min(k,n-1);i++){
                     inv = (inv + dp[n-1][k-i])%1000000007;
