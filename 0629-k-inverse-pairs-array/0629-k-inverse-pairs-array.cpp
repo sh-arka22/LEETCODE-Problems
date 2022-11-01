dd45be9864a3fb3146be292c8a299c81607f6072
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-11-02 04:33:19 +0530

    Time: 1532 ms (13.66%), Space: 10.3 MB (75.90%) - LeetHub

--- 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.cpp
@@ -1,14 +1,5 @@
 class Solution {
 private:
-    vector<vector<int>>dp;
-    // int kInversePairs(int n, int k) {
-    //     int dp[1001][1001] = { 1 };
-    //     for (int N = 1; N <= n; ++N)
-    //         for (int K = 0; K <= k; ++K)
-    //             for (int i = 0; i <= min(K, N - 1); ++i) 
-    //                 dp[N][K] = (dp[N][K] + dp[N - 1][K - i]) % 1000000007;
-    //     return dp[n][k];
-    // }
     int recc(int N, int K){
         int dp[1001][1001] = { 1 };
         for(int n=0;n<=N;n++){
@@ -32,7 +23,6 @@ private:
     }
 public:
     int kInversePairs(int n, int k) {
-        dp.resize(1001, vector<int>(1001,1));
         return recc(n, k);
     }
 };
