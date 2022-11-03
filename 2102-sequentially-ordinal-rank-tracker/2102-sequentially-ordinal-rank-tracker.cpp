8156f1c79ef5b2ad16104ffdeec1d61e3080cffe
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-11-03 08:28:25 +0530

    Time: 859 ms (62.37%), Space: 149.4 MB (17.56%) - LeetHub

--- 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.cpp
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.cpp
@@ -1,28 +1,14 @@
-class SORTracker  {
+class SORTracker {
 public:
-    using iter = set<pair<int, string>>::iterator;
     set<pair<int, string>> s;
-    iter it = end(s);    
+    set<pair<int, string>>::iterator it = end(s);    
     void add(string name, int score) {
-        auto it1 = s.insert({-score, name}).first;//returns pair whre first points to position and second is bool
-        //logic of converting score to -score is to get the best city in front
-        //if (it == end(s) || *it1 < *it)
-        if (it == end(s) || comp(it1, it))
-        {
+        auto it1 = s.insert({-score, name}).first;
+        if (it == end(s) || *it1 < *it)
             --it;
-        }
     }
     string get() {
         return (it++)->second;
-    }
-    bool comp(iter &a, iter &b) {
-        if(a->first < b->first) {
-            return true;
-        }
-        else if(a->first == b->first && a->second < b->second) {
-            return true;
-        }
-        return false;
     }
 };
 
