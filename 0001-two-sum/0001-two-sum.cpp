317f978160eca35899524e06902c2836149d69d2
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-11-14 17:51:25 +0530

    Time: 34 ms (53.62%), Space: 10.9 MB (44.90%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int tar);
+};
+
+vector<int> Solution::twoSum(vector<int> &nums, int tar){
+    unordered_map<int,int>mp;
+    int n = nums.size();
+    for(int i=0;i<n;i++){
+        int y = nums[i];
+        int x = tar-y;
+        if(mp.count(x)){
+            return {mp[x],i};
+        }
+        mp[y] = i;
+    }
+
+    return {};
+}
