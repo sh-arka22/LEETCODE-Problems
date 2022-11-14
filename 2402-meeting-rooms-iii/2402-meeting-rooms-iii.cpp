9d2c520eca934c26041e6b6dafa4884cdaa5199f
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-11-14 17:51:44 +0530

    Time: 875 ms (74.23%), Space: 96.7 MB (83.12%) - LeetHub

---
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        using ll = long long;
+        sort(begin(meetings), end(meetings));
+        auto cmp = [](array<ll,2>&a, array<ll,2>&b){
+            return (a[0]==b[0]? a[1]>b[1] : a[0]>b[0]);
+        };
+        priority_queue<array<ll,2>, vector<array<ll,2>>, decltype(cmp)>pq(cmp);
+        for(int i=0;i<n;i++) pq.push({0,i});
+        vector<ll>freq(n,0);
+        for(auto &meet:meetings){
+            while(pq.top()[0]<meet[0]){
+                pq.push({meet[0], pq.top()[1]});
+                pq.pop();
+            }
+            auto [dur, idx] = pq.top();
+            dur += (meet[1]-meet[0]);
+            pq.push({dur, idx});
+            freq[idx]++;
+            pq.pop();
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(freq[i]>freq[ans]){
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
