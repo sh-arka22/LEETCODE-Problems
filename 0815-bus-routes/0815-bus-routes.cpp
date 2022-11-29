726ac939ba6f524b0160ef4d3b383a7c11076103
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-11-29 17:41:28 +0530

    Time: 644 ms (26.00%), Space: 66.9 MB (28.28%) - LeetHub

---
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numBusesToDestination(vector<vector<int>>& routes, int src, int tar) {
+        map<int,vector<int>>to_routes;
+        for(int i=0;i<routes.size();i++){
+            for(int stops: routes[i]){
+                to_routes[stops].push_back(i);
+            }
+        }
+        set<int>vis;
+        vis.insert(src);
+        deque<int>que;
+        que.push_back(src);
+        int rad = 0;
+        while(que.size()){
+            int sz = que.size();
+            while(sz--){
+                int stop = que.front();
+                que.pop_front();
+                if(stop == tar) return rad;
+                for(int path: to_routes[stop]){
+                    for(int station: routes[path]){
+                        if(vis.count(station)) continue;
+                        que.push_back(station);
+                        vis.insert(station);
+                    }
+                    routes[path].clear();
+                }
+            }
+            rad++;
+        }
+        return -1;
+    }
+};
