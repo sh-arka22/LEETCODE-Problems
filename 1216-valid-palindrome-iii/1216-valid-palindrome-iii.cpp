d28626b404886ae0536c92a06859acdb7b2d73c1
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-11-30 09:26:03 +0530

    Time: 25 ms (86.61%), Space: 17 MB (9.45%) - LeetHub

---
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int recc(string &s, int st, int en, vector<vector<int>>&dp){
+        if(st>=en) return dp[st][en] = (st == en ? 1 : 0);
+        if(dp[st][en] != -1) return dp[st][en];
+        int cnt;
+        if(s[st] != s[en]){
+            cnt = max(recc(s,st+1,en,dp),recc(s,st,en-1,dp));
+        }
+        else cnt = (recc(s,st+1,en-1,dp)+2);
+
+        return dp[st][en] = cnt;
+    }
+
+    int longestPalindromeSubseq(string s) {
+        vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
+        return recc(s,0,s.size()-1,dp);
+    }
+public:
+    bool isValidPalindrome(string s, int k) {
+        int n = size(s);
+        return n-longestPalindromeSubseq(s) <= k;
+    }
+};
