194b0661fd9d2db5a6e5575a6bcee95713638057
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-12-30 22:23:06 +0530

    Time: 2570 ms (5.04%), Space: 431.1 MB (6.14%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,24 +1,26 @@
 class Solution {
 private:
-    vector<vector<vector<int>>>dp;
-    int recc(vector<int>& prices, int idx, int k, int state, int n){
-        if(n == idx or k == 0) return dp[idx][k][state] = 0;
-        if(dp[idx][k][state] != -1) return dp[idx][k][state];
-        int profit;
-        if(state == 0){
-            // i can buy;
-            profit = max(recc(prices, idx+1, k, 0, n), -prices[idx] + recc(prices, idx+1, k, 1, n));
-        }
-        else if(state == 1){
-            //i can sell
-            profit = max(recc(prices, idx+1, k, 1, n), prices[idx]+ recc(prices, idx+1, k-1, 0, n));
-        }
-        return dp[idx][k][state] = profit;
-    } 
+    unordered_map<string, int>mp;
+	int recc(vector<int> prices, int idx, int state, int k){
+		string key = to_string(idx) + "|" + to_string(state) + "|" + to_string(k);
+		if(idx == prices.size() or k == 0) return 0;
+		if(mp[key] != 0) return mp[key];
+		int profit = 0;
+		if(state == 0){
+			// i can buy
+			profit = max(recc(prices, idx+1, 0, k), recc(prices, idx+1, 1, k) - prices[idx]);
+		}
+		else if(state == 1){
+			// k-1
+			// i know ican only sell
+			profit = max(recc(prices, idx+1, 1, k), prices[idx] + recc(prices, idx+1, 0, k-1));
+		}
+		mp[key] = profit;
+		return profit;
+	} 
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        dp.resize(n+1, vector<vector<int>>(k+1, vector<int>(2,-1)));
-        return recc(prices, 0, k, 0, n);
+        return recc(prices, 0, 0, k);
     }
 };
