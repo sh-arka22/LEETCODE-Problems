f11c7c5a0f2e0e3dcecc86a5bab1c3c9fc1dee1c
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-12-31 22:23:29 +0530

    Time: 20 ms (77.96%), Space: 16.8 MB (99.41%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    void insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int index = upper_bound(intervals.begin(), intervals.end(), newInterval) - intervals.begin();
+        
+        if (index != intervals.size()) {
+            intervals.insert(intervals.begin() + index, newInterval);
+        } else {
+            intervals.push_back(newInterval);
+        }
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        sort(begin(intervals), end(intervals));
+        int n = size(intervals);
+        ans.push_back(intervals[0]);
+        for(int i=1; i<n; i++){
+            if(intervals[i][0]<=ans.back()[1]){
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+            else ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        insertInterval(intervals, newInterval);
+        return merge(intervals);
+    }
+};
