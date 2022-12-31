1b5eae9e3f6f6e2596bf18b616ab157033d5074d
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2022-12-31 23:57:19 +0530

    Time: 501 ms (78.56%), Space: 89.9 MB (28.82%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        auto cmp = [](vector<int>&a ,vector<int>&b){
+            return a[1]<b[1];
+        };
+        sort(begin(intervals), end(intervals),cmp);
+        int e = intervals[0][1];
+        int n = size(intervals);
+        int cnt = 0;
+        for(int i=1;i<n;i++){
+            if(intervals[i][0]<=e){
+                cnt ++;
+            }
+            else{
+                e = intervals[i][1];
+            }
+        }
+        return cnt;
+    }
+public:
+    //leetcode 435;
+    int findMinArrowShots(vector<vector<int>>& points) {
+        return size(points) - eraseOverlapIntervals(points);
+    }
+};
