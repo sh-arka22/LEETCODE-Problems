afd866335d0d231969553defda73156aae96d5bf
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2023-01-01 15:53:40 +0530

    Time: 24 ms (42.85%), Space: 16.2 MB (10.68%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,24 +1,47 @@
+class Trie{
+      public:
+        Trie *child[26];
+        bool isEnd = false;
+    };
+
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set <string> st;
-        int maxi = 0;
-        int n = s.size();
-        for(string it:wordDict){
-            st.insert(it);
-            maxi = max(maxi, (int)it.size());
+    void insert(string &word, Trie* root){
+        Trie *cur = root;
+        for(auto &ch : word){
+            if(!cur->child[ch-'a']) cur->child[ch-'a'] = new Trie();
+            cur=cur->child[ch-'a'];
         }
-        cout<<maxi;
-        vector<int>dp(n+1,0);
-        dp[0] = 1;
-        for(int i=0;i<=n;i++){
-            if(dp[i]==0) continue;
-            for(int len = 1; len<=maxi and i+len<=n;len++){
-                if(st.count(s.substr(i,len))){
-                    dp[i+len] = 1;
-                }
+        cur->isEnd = true;
+    }
+    
+    bool search(string &word, Trie* root){
+        Trie *cur = root;
+        for(auto &ch : word){
+            if(!cur->child[ch-'a']) return false;
+            cur=cur->child[ch-'a'];
+        }
+        return cur->isEnd;
+    }
+    
+    int dp[305][305];
+    bool solve(string &s, Trie *root, int n, int start){
+        if(start==n) return true;
+        if(dp[start][n]!=-1) return dp[start][n];
+        for(int i=start; i<n; ++i){
+            string str = s.substr(start,i-start+1);
+            if(search(str,root)){
+                if(solve(s,root,n,i+1)) return dp[start][n] = true;
             }
         }
-        return dp[n];
+        return dp[start][n] = false;
+    }
+    
+    bool wordBreak(string &s, vector<string>& wordDict) {
+        Trie *root = new Trie();
+        for(auto &word : wordDict) insert(word,root);
+        int n=s.size();
+        memset(dp,-1,sizeof(dp));
+        return solve(s,root,n,0);
     }
 };
