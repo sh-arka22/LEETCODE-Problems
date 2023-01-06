183f5c10654a9c291b96ef09f22558aae12e428c
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2023-01-07 00:13:41 +0530

    Time: 195 ms (66.60%), Space: 16.4 MB (68.61%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int mem[100001][2][3] = {};
     int checkRecord(int n) {
-        return dfs(0, 0, 0, n, mem);   
+        return dfs(0, 0, 0, n);   
     }
 
 private:
-    int dfs(int i, int A, int L, int n, int mem[][2][3]) {
+    int dfs(int i, int A, int L, int n) {
         if (i == n) return 1;
         if (mem[i][A][L] != 0) return mem[i][A][L];
-        long int res = dfs(i+1, A, 0, n, mem);  // P
-        if (A == 0) res += dfs(i+1, 1, 0, n, mem); // A
-        if (L < 2) res += dfs(i+1, A, L+1, n, mem); // L
+        long int res = dfs(i+1, A, 0, n);  // P
+        if (A == 0) res += dfs(i+1, 1, 0, n); // A
+        if (L < 2) res += dfs(i+1, A, L+1, n); // L
         return mem[i][A][L] = res % 1000000007;
     }
 };
