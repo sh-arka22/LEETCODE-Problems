6aec86e8ed14b4d40d192d267017b752578bb444
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2023-01-13 00:11:46 +0530

    Time: 4 ms (37.45%), Space: 6.3 MB (68.93%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    vector<int>kmp(string txt, string pat){
+        //making of pi table
+        int n = txt.size();
+        int m = pat.size();
+        
+        vector<int>pi(m,0);
+        int j = 0;
+        for(int i=1;i<m;i++){
+            while(j>0 and pat[i] != pat[j]){
+                j = pi[j-1];
+            }
+            if(pat[j] == pat[i])
+                j++;
+            pi[i] = j;
+        }
+        
+        vector<int>occurance;
+        j=0;
+        for(int i=0;i<n;i++){
+            while(j>0 and pat[j] != txt[i]){
+                j = pi[j-1];
+            }
+            if(pat[j] == txt[i])
+                j++;
+            if(j == m){
+                occurance.push_back(i-m+1);
+                j = pi[j-1];
+            }
+        }
+        return occurance;
+    }
+public:
+    int strStr(string haystack, string needle) {
+        return kmp(haystack, needle).size() ? kmp(haystack, needle)[0] : -1;
+    }
+};
