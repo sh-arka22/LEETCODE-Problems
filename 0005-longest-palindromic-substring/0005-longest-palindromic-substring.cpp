bfe7b96975a3084352059bbb5d1ac1e798d2469a
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2023-01-17 00:24:53 +0530

    Time: 6 ms (96.71%), Space: 8.4 MB (57.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        string res = "";
-        int len = 0;
-        for(int i=0;i<n;i++){
-            
-            int l = i, r = i;
-            while(l>=0 and r<n and s[l]==s[r]){
-                if(len < (r-l+1)){
-                    len = r-l+1;
-                    res = s.substr(l,len);
-                }
-                l--, r++;
-            }
-            l = i-1, r = i;
-            while(l>=0 and r<n and s[l]==s[r]){
-                if(len < (r-l+1)){
-                    len = r-l+1;
-                    res = s.substr(l,len);
-                }
-                l--, r++;
-            }
-        }
-        return res;
+    int n = s.size();
+    string t = "#";
+    for (int i = 0; i < n; i++) {
+        t += s[i];
+        t += "#";
     }
+    n = t.size();
+
+    vector<int> p(n, 0);
+    int c = 0, r = 0;
+    int maxLen = 0, center = 0;
+    for (int i = 0; i < n; i++) {
+        int i_mirror = 2 * c - i;
+        if (i < r) {
+            p[i] = min(r - i, p[i_mirror]);
+        }
+
+        int a = i + (1 + p[i]), b = i - (1 + p[i]);
+        while (a < n && b >= 0 && t[a] == t[b]) {
+            p[i]++;
+            a++;
+            b--;
+        }
+
+        if (i + p[i] > r) {
+            c = i;
+            r = i + p[i];
+        }
+        if (p[i] > maxLen) {
+            maxLen = p[i];
+            center = i;
+        }
+    }
+
+    return s.substr((center - maxLen) / 2, maxLen);
+}
 };
