136161cf8ce57756d62422fddeee1ee4e0fbccd8
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2023-02-08 22:25:21 +0530

    Time: 309 ms (91.11%), Space: 78.5 MB (91.11%) - LeetHub

--- 2021-brightest-position-on-street/2021-brightest-position-on-street.cpp
+++ 2021-brightest-position-on-street/2021-brightest-position-on-street.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int brightestPosition(vector<vector<int>>& lights) {
-        map<int,int> count;
-        for(auto interval:lights){
-            int start=interval[0]-interval[1];
-            int end=interval[0]+interval[1];
-            count[start]++;
-            count[end+1]--;
+        vector<pair<int, int>> line;
+        for (auto &l : lights) {
+            line.push_back({l[0] - l[1], 1});
+            line.push_back({l[0] + l[1] + 1, -1});
         }
-        int maxi = 0;
-        int res_ind=1;
-        int sum = 0;
-        for(auto [idx, val]:count){
-            int cur = sum + val;
-            sum = cur;
-            if(cur > maxi){
-                maxi = cur;
-                res_ind = idx;
+        int bright = 0, max_bright = 0, res = INT_MIN;
+        sort(begin(line), end(line));
+        for (auto [pos, light] : line) {
+            bright += light;
+            if (bright > max_bright) {
+                max_bright = bright;
+                res = pos;
             }
         }
-        return res_ind;
+        return res;
     }
 };
