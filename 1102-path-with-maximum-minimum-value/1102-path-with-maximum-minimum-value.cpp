9a5973f4d91cf98a50163ff79345d7152a4050c2
Arkajyoti Saha <91637787+sh-arka22@users.noreply.github.com>
2023-06-16 22:53:09 +0530

    Time: 264 ms (87.67%), Space: 31.2 MB (76.03%) - LeetHub

--- 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
+++ 1102-path-with-maximum-minimum-value/1102-path-with-maximum-minimum-value.cpp
@@ -1,48 +1,26 @@
 class Solution {
 public:
-    int dir[4][2] = {{-1,0},{0,-1},{1,0},{0,1}};
-    int vis[100][100];
-    int N, M;
-    void visClear(){
-        for(int i=0;i<100;i++){
-            for(int j=0;j<100;j++){
-                vis[i][j] = 0;
-            }
-        }
-    }
-    
-    bool dfs(vector<vector<int>>& grid, int i, int j, int tar){
-        if(i == N-1 and j == M-1) return true;
-        if(grid[i][j] < tar) return false;
-        vis[i][j] = 1;
-        bool flag = false;
-        for(auto &[dr,dc]:dir){
-            int r = i+dr, c = j+dc;
-            if(r<0 or c<0 or r>=N or c>=M or vis[r][c] or grid[r][c]<tar) continue;
-            flag = flag or dfs(grid, r, c, tar);
-        }
-        return flag;
-    }
     int maximumMinimumPath(vector<vector<int>>& grid) {
-        N = grid.size();
-        M = grid[0].size();
-        
-        int l = 0, r = 1e9;
-        
-        int mid;
-        int ans = -11;
-        while(l<=r){
-            mid = (r+l)/2;
-            // cout<< mid<<endl;
-            bool flag = dfs(grid, 0, 0, mid);
-            
-            if(flag){
-                ans = max(ans,mid);
-                l = mid+1;
+        int n = size(grid), m = size(grid[0]);
+        int dirs[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        priority_queue<pair<int,int>> pq;
+        pq.push({grid[0][0], 0});
+        vector<int>vis(n*m, 0);
+        vis[0] = 1;
+        int ans = grid[0][0];
+
+        while(pq.size()){
+            auto [curVal, idx] = pq.top();
+            pq.pop();
+            ans = min(ans, curVal);
+            int r = idx/m, c = idx%m;
+            if(r == n-1 and c == m-1) return ans;
+            for(auto [dr, dc]: dirs){
+                int newR = r+dr, newC = c+dc;
+                if(newR<0 or newC<0 or newR>=n or newC>=m or vis[newR*m+newC]) continue;
+                pq.push({grid[newR][newC], newR*m+newC});
+                vis[newR*m+newC] = 1;
             }
-            else r = mid-1;
-            
-            visClear();
         }
         return ans;
     }
